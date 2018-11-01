85c7d584afa2b8c228f7e4d40b31d2bd05f0739e
feildmaster <git@feildmaster.com>
2018-11-01 17:11:40 -0500

    build meta with script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-const { dest, parallel, series, src, watch } = require('gulp');
+const argv = require('minimist')(process.argv.slice(2), { boolean: true, });
+const { dest, parallel, src, watch } = require('gulp');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 
-function build(cb) {
-  return src(['./src/meta.js', './src/base/*.js', './src/desktop/*.js', './src/hooks/*.js'])
+const metafile = './src/meta.js';
+const underscript = [metafile, './src/base/*.js', './src/desktop/*.js', './src/hooks/*.js'];
+
+function buildMeta() {
+  return src(metafile)
+    .pipe(rename('undercards.meta.js'))
+    .pipe(docs());
+}
+
+function build() {
+  return src(underscript)
     .pipe(concat('undercards.user.js'))
     .pipe(docs());
 }
@@ -12,7 +22,9 @@ function docs() {
   return dest('./docs');
 }
 
-watch(['./src/underscript.js'], build);
+if (!argv.deploy) {
+  watch(metafile, buildMeta)
+  watch(underscript, build);
+}
 
-exports.build = build;
-exports.default = parallel(build);
+exports.default = parallel(buildMeta, build);
