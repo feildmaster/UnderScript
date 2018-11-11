b71326ffe38f92a9a218b777feb0e0b5d0f92d10
feildmaster <git@feildmaster.com>
2018-11-10 22:47:17 -0600

    Need to check who's turn it is

--- src/base/endTurnDelay.js
+++ src/base/endTurnDelay.js
@@ -1,5 +1,6 @@
 eventManager.on('PlayingGame', function endTurnDelay() {
   eventManager.on('getTurnStart', function checkDelay() {
+    if (userTurn !== userId) return;
     if (turn > 3 && !localStorage.getItem('setting.disable.endTurnDelay')) {
       $('#endTurnBtn').prop('disabled', true);
       setTimeout(() => {
