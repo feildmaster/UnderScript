f612c905163c156cfcd7e229462125492af5a421
feildmaster <git@feildmaster.com>
2018-11-11 13:46:36 -0600

    Pending ignore list settings

--- src/base/ignoreList.settings.js
+++ src/base/ignoreList.settings.js
@@ -1,5 +1,27 @@
 settings.register({
   name: 'Disable Ignore Chat',
   key: 'underscript.disable.ignorechat',
+  page: 'ignorelist',
   disabled: true,
 });
+
+settings.register({
+  name: 'Ignore Behavior',
+  key: 'underscript.ignorechat.how',
+  type: 'select',
+  options: ['remove', 'bulk', 'hide'],
+  page: 'ignorelist',
+  disabled: true,
+});
+
+Object.keys(localStorage).forEach((key) => {
+  if (!key.startsWith('underscript.ignore.')) return;
+  settings.register({
+    key,
+    name: localStorage.getItem(key),
+    type: 'remove',
+    page: 'ignorelist',
+  });
+});
+
+settings.setDisplayName('Ignore List', 'ignorelist');
