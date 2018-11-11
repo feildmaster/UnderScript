3ec835f61f4c3dc5cc697e1ee68070851ddb1349
feildmaster <git@feildmaster.com>
2018-11-11 12:42:50 -0600

    End turn hotkey settings
    Hotkeys deserve their own page with the ability to keybind

--- src/base/endTurn.hotkey.js
+++ src/base/endTurn.hotkey.js
@@ -1,12 +1,36 @@
+settings.register({
+  name: 'Disable End Turn Hotkey',
+  key: 'underscript.disable.endTurn',
+});
+settings.register({
+  name: 'Disable End Turn with Space',
+  key: 'underscript.disable.endTurn.space',
+  disabled: () => localStorage.getItem('underscript.disable.endTurn') === '1',
+  note: () => {
+    if (typeof gameId === 'undefined') return;
+    return 'Will require you to refresh the page';
+  },
+});
+settings.register({
+  name: 'Disable End Turn with Middle Click',
+  key: 'underscript.disable.endTurn.middleClick',
+  disabled: () => localStorage.getItem('underscript.disable.endTurn') === '1',
+  note: () => {
+    if (typeof gameId === 'undefined') return;
+    return 'Will require you to refresh the page';
+  },
+});
+
 eventManager.on("PlayingGame", function bindHotkeys() {
   // Binds to Space, Middle Click
   const hotkey = new Hotkey("End turn").run((e) => {
+      if (localStorage.getItem('underscript.disable.endTurn')) return;
       if (!$(e.target).is("#endTurnBtn") && userTurn === userId) endTurn();
     });
-  if (!localStorage.getItem('setting.disable.endTurn.space')) {
+  if (!localStorage.getItem('underscript.disable.endTurn.space')) {
     hotkey.bindKey(32);
   }
-  if (!localStorage.getItem('setting.disable.endTurn.middleClick')) {
+  if (!localStorage.getItem('underscript.disable.endTurn.middleClick')) {
     hotkey.bindClick(2);
   }
   hotkeys.push(hotkey);
