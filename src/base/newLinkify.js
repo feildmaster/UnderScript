b15bea8dd715614d6acb33897d11a4fef4f3662a
feildmaster <git@feildmaster.com>
2018-11-13 16:33:36 -0600

    Setting to disable new linkify...
    I don't like this at all

--- src/base/newLinkify.js
+++ src/base/newLinkify.js
@@ -1,4 +1,16 @@
+settings.register({
+  name: 'Use Original Link Detection',
+  key: 'underscript.disable.linkify',
+  category: 'chat',
+});
+
 eventManager.on('ChatDetected', () => {
   const regex = /((?:https?:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi
-  linkify = (text) => text.replace(regex, `<a href="$1" onclick="return link('$1') || false;">$1</a>`);
+  const oLinkify = linkify;
+  linkify = (text) => {
+    if (settings.value('underscript.disable.linkify')) {
+      return oLinkify(text);
+    }
+    return text.replace(regex, `<a href="$1" onclick="return link('$1') || false;">$1</a>`)
+  };
 });
