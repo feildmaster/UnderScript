56549352c8d4fd078d535971ed8445c538e05e50
feildmaster <git@feildmaster.com>
2018-11-15 19:01:13 -0600

    feat(chat): Replace chat notification function

---
+++ src/base/notify.new.js
@@ -0,0 +1,33 @@
+settings.register({
+  name: 'Disable Chat Ping <span style="color: yellow;">(highlighting)</span>',
+  key: 'underscript.disable.ping',
+  page: 'Chat',
+});
+
+settings.register({
+  name: 'Use Original Chat Ping Detection',
+  key: 'underscript.disable.notify',
+  page: 'Chat',
+});
+
+eventManager.on('ChatDetected', () => {
+  const mask = '<span style="color: yellow;">$1</span>';
+  
+  const oNotify = notif;
+  notif = (text) => {
+    if (!settings.value('underscript.disable.ping')) {
+      if (settings.value('underscript.disable.notify')) {
+        return oNotify(text);
+      }
+      const exp = `\\b((?:${[selfUsername].concat(settings.value('underscript.ping.extras')).join(')|(?:')}))\\b`;
+      const regex = new RegExp(exp, 'gi');
+      if (regex.test(text)) {
+        if (soundsEnabled) {
+          (new Audio("sounds/highlight.wav")).play();
+        }
+        return text.replace(regex, mask);
+      }
+    }
+    return text;
+  };
+});
