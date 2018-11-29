5356cc656e8d79c6b51c65037dcfe77e4bce1728
feildmaster <git@feildmaster.com>
2018-11-29 01:42:29 -0600

    feat: Average gold counter for Decks

---
+++ src/base/deck.average.js
@@ -0,0 +1,30 @@
+settings.register({
+  name: 'Disable deck average counter',
+  key: 'underscript.disable.deck.average',
+  refresh: onPage('Decks'),
+});
+
+// Calculate average
+onPage('Decks', () => {
+  eventManager.on('jQuery', () => {
+    const reg = /^.*\((\d+)\)/;
+    const avg = $('<span>').hover(hover.show('Average gold cost'));
+    $('#decksSizeJUSTICE').after(' ', avg);
+
+    function round(amt, dec = 2) {
+      return Number.parseFloat(amt).toFixed(dec)
+    }
+
+    function count() {
+      let val = 0;
+      const list = $(`#deckCards${classe} li`);
+      list.each(function () {
+        val += parseInt($(this).text().replace(reg, '$1'));
+      });
+      avg.text(`(${round(list.length ? val / list.length : val)})`);
+    }
+
+    count();
+    eventManager.on('Deck:Soul Deck:Change', count);
+  });
+});
