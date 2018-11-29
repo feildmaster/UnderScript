c38e56219e952f9d282db9eccccab17260a7b5d7
feildmaster <git@feildmaster.com>
2018-11-29 01:22:19 -0600

    feat: Quest Highlight and Notification

---
+++ src/base/quest.highlight.js
@@ -0,0 +1,63 @@
+settings.register({
+  name: 'Disable Quest Completed Notifications',
+  key: 'underscript.disable.questHighlight',
+});
+
+(() => {
+  if (settings.value('underscript.disable.questHighlight')) return;
+
+  function highlightQuest() {
+    if (sessionStorage.getItem('questCleared')) {
+      $('a[href="Quests"]').addClass('highlightQuest');
+    }
+  }
+
+  if (!sessionStorage.getItem('questCleared')) {
+    function checkHighlight() {
+      axios.get('/Quests').then(function (response) {
+        const data = $(response.data);
+        const quests = data.find('input[name="dailyQuest"]:not(:disabled)');
+        if (quests.length) {
+          sessionStorage.setItem('questCleared', true);
+          if (onPage('Game')) {
+            let questsCleared = '';
+            quests.each((i, e) => questsCleared += `- ${$(e).parent().text().trim()}\n`);
+            fn.toast({
+              title: 'Quest Completed!',
+              text: `${questsCleared}Click to go to Quests page`,
+              onClose: () => {
+                location.href = '/Quests';
+              },
+            });
+          } else {
+            highlightQuest();
+          }
+        }
+      }).catch(() => {});
+    }
+
+    if (!sessionStorage.getItem('questSkipCheck')) {
+      onPage('', checkHighlight);
+    }
+    eventManager.on('getVictory getDefeat', checkHighlight);
+  }
+
+  eventManager.on('jQuery', function questHighlight() {
+    $('head').append($('<style>').html('a.highlightQuest {color: gold !important;}'));
+
+    const quests = $('a[href="Quests"]');
+    if (quests.length) {
+      if (!!~quests.text().indexOf('(0)')) {
+        sessionStorage.setItem('questSkipCheck', true);
+      } else {
+        sessionStorage.removeItem('questSkipCheck');
+      }
+    }
+
+    if (onPage('Quests') && !$('input[name="dailyQuest"]:not(:disabled)').length) {
+      sessionStorage.removeItem('questCleared');
+    }
+
+    highlightQuest();
+  });
+})();
--- src/meta.js
+++ src/meta.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         UnderCards script
 // @description  Various changes to undercards game
+// @require      https://unpkg.com/axios@0.18.0/dist/axios.min.js
 // @require      https://raw.githubusercontent.com/feildmaster/SimpleToast/1.12.0/simpletoast.js
 // @version      0.20.0-alpha.0
 // @author       feildmaster
+// @history   0.20.0 - Added "Quest Completion" notifications.
 // @history   0.19.2 - Fixed mod muting
 // @history   0.19.1 - Fixed link detection to not randomly turn www into links. Do it somewhat sane at least
 // @history   0.19.0 - Added Menu and Settings. Fixed unignoring. Added settings to disable nearly everything in the script.
