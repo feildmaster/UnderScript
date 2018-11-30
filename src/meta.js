07de31e49a5e8ad2d6cfac53b19d1b5c0e55eca5
feildmaster <git@feildmaster.com>
2018-11-29 22:02:59 -0600

    chore: pre-0.20.0

--- src/meta.js
+++ src/meta.js
@@ -2,7 +2,7 @@
 // @name         UnderCards script
 // @description  Various changes to undercards game
 // @require      https://raw.githubusercontent.com/feildmaster/SimpleToast/1.12.0/simpletoast.js
-// @version      0.19.1
+// @version      0.20.0-alpha.0
 // @author       feildmaster
 // @history   0.19.1 - Fixed link detection to not randomly turn www into links. Do it somewhat sane at least
 // @history   0.19.0 - Added Menu and Settings. Fixed unignoring. Added settings to disable nearly everything in the script.
