7c0fd7ef5c69aa459a641e0c93cb9a722e012fe5
feildmaster <git@feildmaster.com>
2018-12-06 20:36:36 -0600

    feat: Automatically refresh game list
    I should really calculate the refresh rate, rather than simply every 10 seconds.

---
+++ src/base/indexRefresh.js
@@ -0,0 +1,33 @@
+onPage('', function refreshGameList() {
+  let id;
+  let refreshing = false;
+  function refresh(timeout) {
+    if (timeout) id = 0;
+    if (refreshing || document.visibilityState === 'hidden') return;
+    refreshing = true;
+    axios.get('/').then((response) => {
+      const data = $(response.data);
+      const list = data.find('#liste');
+      const live = $('#liste'); 
+      live.find('tbody').html(list.find('tbody').html());
+      live.prev('p').html(list.prev().html());
+    }).catch((e) => {
+      fn.debug(`Index: ${e.message}`);
+    }).then(() => {
+      refreshing = false;
+      setup();
+    });
+  }
+
+  function setup(delay = 10000) {
+    if (id) {
+      clearTimeout(id);
+    }
+    id = setTimeout(refresh, delay, true);
+  }
+
+  // Restart refresh sequence when returning to page
+  document.addEventListener('visibilitychange', refresh);
+  // Queue initial refresh
+  setup();
+});
