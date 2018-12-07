90cd2690f5fdb0da021cf92bc6e87073b9504583
feildmaster <git@feildmaster.com>
2018-12-06 19:20:32 -0600

    0.19.2

--- src/meta.js
+++ src/meta.js
@@ -2,7 +2,7 @@
 // @name         UnderCards script
 // @description  Various changes to undercards game
 // @require      https://raw.githubusercontent.com/feildmaster/SimpleToast/1.12.0/simpletoast.js
-// @version      0.19.1
+// @version      0.19.2
 // @author       feildmaster
 // @history   0.19.2 - Fixed mod muting
 // @history   0.19.1 - Fixed link detection to not randomly turn www into links. Do it somewhat sane at least
