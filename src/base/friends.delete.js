c00eacfd5f802ac8dc6e81d3a7165cf8035da3d4
feildmaster <git@feildmaster.com>
2018-12-09 06:35:05 -0600

    feat: Remove friends easier

---
+++ src/base/friends.delete.js
@@ -0,0 +1,37 @@
+settings.register({
+  name: 'Remove friends without refreshing',
+  key: 'underscript.removeFriend.background',
+  default: true,
+})
+
+onPage('Friends', function deleteFriends() {
+  style.add('.deleted { text-decoration: line-through; }');
+
+  function remove(e) {
+    if (!settings.value('underscript.removeFriend.background')) return;
+    e.preventDefault();
+    const btn = $(this);
+    const parent = btn.parent();
+    btn.detach();
+    const link = btn.attr('href');
+    axios.get(link).then((response) => {
+      const onlineFriends = $(response.data).find(`#onlineFriends`);
+      if (!onlineFriends.length) {
+        // Error
+        fn.toast('Error: Try logging back in');
+        return;
+      }
+      if (onlineFriends.find(`a[href="${link}"]`).length) {
+        // This "name" detection isn't good
+        fn.toast(`Failed to remove: ${parent.find('span:nth-child(3)').text()}`);
+        btn.appendTo(parent);
+      } else {
+        parent.addClass('deleted');
+      }
+    }).catch((e) => {
+      fn.debug(`DeleteFriend: ${e}`)
+    });
+  }
+
+  eventManager.on('Chat:getOnlineFriends', () => $('a.crossDelete').click(remove));
+});
