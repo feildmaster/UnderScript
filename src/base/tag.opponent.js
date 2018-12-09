df8dffd3e621601353487450ded6d3c30cc57ad4
feildmaster <git@feildmaster.com>
2018-12-08 20:50:29 -0600

    feat: Highlight your opponents in chat
    Now you know if your opponent is trash talking you at a glance

---
+++ src/base/tag.opponent.js
@@ -0,0 +1,24 @@
+settings.register({
+  name: 'Highlight <span style="color: #d94f41">opponents</span> in chat',
+  key: 'underscript.tag.opponent',
+  default: true,
+  page: 'Chat',
+});
+
+eventManager.on('PlayingGame', function tagOpponent() {
+  style.add('.opponent { color: #d94f41 !important; }');
+  function processMessage(message, room) {
+    if (!finish && message.user.id === opponentId) {
+      $(`#${room} #message-${message.id} .chat-user`).addClass('opponent');
+    }
+  }
+
+  eventManager.on('Chat:getHistory', (data) => {
+    JSON.parse(data.history).forEach((message) => {
+      processMessage(message, data.room);
+    });
+  });
+  eventManager.on('Chat:getMessage', function tagFriends(data) {
+    processMessage(JSON.parse(data.chatMessage), data.room);
+  });
+});
