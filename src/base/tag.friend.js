a2385d063595fed5168368d245346986daffe86b
feildmaster <git@feildmaster.com>
2018-12-08 20:49:37 -0600

    feat: Highlight your friends in chat

---
+++ src/base/tag.friend.js
@@ -0,0 +1,25 @@
+settings.register({
+  name: 'Highlight <span style="color: #77cfac">friends</span> in chat',
+  key: 'underscript.tag.friend',
+  default: true,
+  page: 'Chat',
+});
+
+eventManager.on('ChatDetected', function friendWrapper() {
+  style.add('.friend { color: #77cfac !important; }');
+  function processMessage(message, room) {
+    if (!settings.value('underscript.tag.friend')) return;
+    if (isFriend(message.user.id)) {
+      $(`#${room} #message-${message.id} .chat-user`).addClass('friend');
+    }
+  }
+
+  eventManager.on('Chat:getHistory', (data) => {
+    JSON.parse(data.history).forEach((message) => {
+      processMessage(message, data.room);
+    });
+  });
+  eventManager.on('Chat:getMessage', function tagFriends(data) {
+    processMessage(JSON.parse(data.chatMessage), data.room);
+  });
+});
