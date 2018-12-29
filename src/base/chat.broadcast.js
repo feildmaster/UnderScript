6f4fbcb3a2e946ca2113efcbba468d4cc2303ca5
feildmaster <git@feildmaster.com>
2018-12-29 17:48:19 -0600

    feat: Broadcast messages now toast

---
+++ src/base/chat.broadcast.js
@@ -0,0 +1,13 @@
+settings.register({
+  name: 'Disable Broadcast Toast',
+  key: 'underscript.disable.broadcast',
+});
+
+eventManager.on('Chat:getMessageBroadcast', function broadcast({message}) {
+  if (settings.value('underscript.disable.broadcast')) return;
+  fn.infoToast({
+    title: '[INFO] Undercards Broadcast Message',
+    text: `<span style="color: #ff00ff;">${message}</span>`,
+    footer: 'info-chan via UnderScript',
+  });
+});
