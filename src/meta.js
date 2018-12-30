cf957759efd83a02a5412978cda71f38d3940d0c
feildmaster <git@feildmaster.com>
2018-12-30 11:38:44 -0600

    fix: Only include on game pages

--- src/meta.js
+++ src/meta.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         UnderCards script
 // @description  Various changes to undercards game
+// @version      {{ version }}
+// @author       feildmaster
+// @match        https://*.undercards.net/*
+// @match        https://feildmaster.github.io/UnderScript/*
+// @exclude      https://*.undercards.net/*/*
 // @require      https://unpkg.com/tippy.js@3.3.0/dist/tippy.all.min.js
 // @require      https://unpkg.com/axios@0.18.0/dist/axios.min.js
 // @require      https://raw.githubusercontent.com/feildmaster/SimpleToast/2.0.0/simpletoast.js
 // @require      https://unpkg.com/luxon@1.8.2/build/global/luxon.min.js
 // @require      https://www.googletagmanager.com/gtag/js?id=UA-38424623-4
-// @version      {{ version }}
-// @author       feildmaster
-// @match        https://*.undercards.net/*
-// @match        https://feildmaster.github.io/UnderScript/*
 // @homepage     https://feildmaster.github.io/UnderScript/
 // @source       https://github.com/feildmaster/UnderScript
 // @supportURL   https://github.com/feildmaster/UnderScript/issues
