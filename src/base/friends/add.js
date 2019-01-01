f9bb808dae8482eb37cbf28a0318a3704762b47b
feildmaster <git@feildmaster.com>
2019-01-01 11:19:32 -0600

    feat: Adding friends no longer refreshes page

---
+++ src/base/friends/add.js
@@ -0,0 +1,49 @@
+settings.register({
+  name: 'Add friends without refreshing',
+  key: 'underscript.friend.add',
+  default: true,
+});
+
+onPage('Friends', function addFriend() {
+  const single = true;
+  const input = document.querySelector('input[name="username"]');
+
+  function submit() {
+    if (typeof URLSearchParams === 'undefined' || !settings.value('underscript.friend.add')) return;
+    const name = input.value.trim();
+    if (name) {
+      input.value = '';
+      input.focus();
+      const params = new URLSearchParams();
+      params.append('username', name);
+      params.append('addFriend', 'Add friend');
+      axios.post('/Friends', params).then((results) => {
+        const page = new DOMParser().parseFromString(results.data, "text/html").querySelector('div.mainContent');
+        const result = page.querySelector('form[action="Friends"] + p');
+        if (result) {
+          const success = result.classList.contains('green');
+          debug(result);
+          fn.toast({
+            text: `<p style="color: ${success ? 'green' : 'red'}">${success ? 'Sent' : 'Failed to send'} friend request to ${name}<p>`,
+          });
+          if (success) {
+            const element = $el.text.contains(page.querySelectorAll('a[href^="Friends?delete="]'), `${name} LV`, { mutex, single });
+            $el.text.contains(document.querySelectorAll('p'), 'Pending requests', { single }).parentElement.append(element);
+            eventManager.emit('newElement', element);
+          }
+        }
+      });
+    }
+    return false;
+  }
+  function mutex(el) {
+    return el.parentElement;
+  }
+  
+  input.addEventListener('keydown', (e) => {
+    if ((e.keyCode || e.which) === 13 || [e.code, e.key].contains('Enter')) {
+      if (submit() === false) e.preventDefault();
+    } 
+  });
+  document.querySelector('form[action="Friends"]').onsubmit = submit;
+});
---
+++ src/utils/elementHelper.js
@@ -0,0 +1,51 @@
+const $el = (() => {
+  function containsText(el, text, {mutex, single} = {}) {
+    if (el.forEach) {
+      const ret = [];
+      for (let i = 0; i < el.length; i++) {
+        const element = mutex ? mutex(el[i]) : el[i];
+        if (!!~getText(element).indexOf(text)) {
+          ret.push(element);
+        }
+      }
+      return single ? ret[0] : ret;
+    }
+    return !!~(el.textContent||el.innerText).indexOf(text);
+  }
+  function getText(el) {
+    return el.value || el.textContent || el.innerText;
+  }
+  function setText(el, text) {
+    const set = Object.getOwnPropertyDescriptor(el, 'textContent') ? 'textContent' : 'innerText';
+    el[set] = text;
+    return el;
+  }
+  function containsHTML(el, text, {mutex, single} = {}) {
+    if (el.forEach) {
+      const ret = [];
+      for (let i = 0; i < el.length; i++) {
+        const element = mutex ? mutex(el[i]) : el[i];
+        if (!!~element.innerHTML.indexOf(text)) {
+          ret.push(element);
+        }
+      }
+      return single ? ret[0] : ret;
+    }
+    return !!~el.innerHTML.indexOf(text);
+  }
+  function setHTML(el, text) {
+    el.innerHTML = text;
+    return el;
+  }
+  return {
+    text: {
+      contains: containsText,
+      set: setText,
+      get: getText,
+    },
+    html: {
+      contains: containsHTML,
+      set: setHTML,
+    },
+  };
+})();
