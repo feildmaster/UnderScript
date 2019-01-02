bdf92a7d5ba67a1d83983da68974c6bb8322d070
feildmaster <git@feildmaster.com>
2019-01-02 14:44:18 -0600

    feat: Change title after finding a game
    Added in some code that should stop the change if onu adds it to vanilla

---
+++ src/base/lobby/gameFound.js
@@ -0,0 +1,12 @@
+(() => {
+  const title = document.title;
+  eventManager.on('getWaitingQueue', function updateTitle() {
+    // Title has been modified
+    if (title !== document.title) return;
+    document.title = `Undercards - Match found!`;
+  });
+  eventManager.on('getLeaveQueue', function restoreTitle() {
+    document.title = title;
+  });
+  fn.infoToast('Page title now changes when a match is found.', 'undercards.notice.play.title', '1');
+})();
