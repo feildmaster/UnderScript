3e3c3859a4587d0490d4488ca402ab63b925931c
feildmaster <git@feildmaster.com>
2019-01-02 12:21:13 -0600

    feat: Legendary draw toasts now stack results

--- src/base/chat/legendary.js
+++ src/base/chat/legendary.js
@@ -12,20 +12,38 @@
   let toastIndex = 0;
   eventManager.on('preChat:getMessageAuto', function drawAnnouncement(data) {
     const setting = settings.value('underscript.announcement.draws');
-    if (this.canceled || setting === 'chat' || !data.message.includes('has just obtained')) return;
+    const index = data.message.indexOf(' has just obtained ');
+    if (this.canceled || setting === 'chat' || !~index) return;
     const both = setting === 'both';
     this.canceled = !both;
     if (both || setting === 'toast') {
+      const owner = data.message.substring(0, index);
+      const card = data.message.substring(index + 19, data.message.length - 2);
+
+      const last = toastIndex > 0 && toasts[(toastIndex - 1)%3];
+      if (last && last.exists() && (true || last.time + 1000 > Date.now())) {
+        if (owner === last.owner) {
+          const text = `${owner} has just obtained ${last.cards.join(', ')} and ${card} !`;
+          last.setText(text);
+          last.cards.push(card);
+          return;
+        }
+      }
       if (toasts[toastIndex % 3]) { // Close any old toast
         toasts[toastIndex % 3].close();
       }
-      toasts[toastIndex % 3] = fn.toast({
+      
+      const toast = fn.toast({
         text: data.message,
         css: {
           color: 'yellow',
           footer: {color: 'white'},
         }
       });
+      toast.cards = [card];
+      toast.owner = owner;
+      toast.time = Date.now();
+      toasts[toastIndex % 3] = toast;
       toastIndex += 1;
     }
   });
