da381fdd42e3ef452f65be87aaa8dd410e214286
feildmaster <git@feildmaster.com>
2019-01-03 10:14:11 -0600

    chore: Update to 30.4

--- src/hooks/game.js
+++ src/hooks/game.js
@@ -7,12 +7,30 @@ onPage("Game", function () {
       debug("Game: Timeout hook");
       return setTimeout(hook);
     }
-    const oHandler = socket.onmessage;
-    socket.onmessage = function onMessageScript(event) {
-      const data = JSON.parse(event.data);
-      eventManager.emit('PreGameEvent', data);
-      oHandler(event);
-      eventManager.emit('GameEvent', data);
-    };
+    function callGameHooks(data, original) {
+        const run = !eventManager.emit('PreGameEvent', data).canceled;
+        try {
+          if (run) original(data);
+        } catch (e) {
+          console.error(e);
+        }
+        eventManager.emit('GameEvent', data);
+    }
+    if (undefined !== bypassQueueEvents) {
+      const oRunEvent = runEvent;
+      const oBypassEvent = bypassQueueEvent;
+      runEvent = function runEventScript(event) {
+        callGameHooks(event, oRunEvent);
+      };
+      bypassQueueEvent = function runEventScript(event) {
+        callGameHooks(event, oBypassEvent);
+      };
+    } else {
+      const oHandler = socket.onmessage;
+      socket.onmessage = function onMessageScript(event) {
+        const data = JSON.parse(event.data);
+        callGameHooks(data, oHandler);
+      };
+    }
   })();
 });
--- src/hooks/spectate.js
+++ src/hooks/spectate.js
@@ -6,12 +6,32 @@ onPage("gameSpectate", function () {
       debug("Spectate: Timeout hook");
       return setTimeout(hook);
     }
-    const oHandler = socket.onmessage;
-    socket.onmessage = function onMessageScript(event) {
-      const data = JSON.parse(event.data);
+    function callGameHooks(data, original) {
       const run = !eventManager.emit('PreGameEvent', data, data.action === 'getResult').canceled;
-      if (run) oHandler(event);
+      try {
+        if (run) original(data);
+      } catch (e) {
+        console.error(e);
+      }
       eventManager.emit('GameEvent', data);
-    };
+    }
+    if (undefined !== window.bypassQueueEvents) {
+      const oRunEvent = runEvent;
+      const oBypassEvent = bypassQueueEvent;
+      runEvent = function runEventScript(event) {
+        callGameHooks(event, oRunEvent);
+      };
+      bypassQueueEvent = function runEventScript(event) {
+        callGameHooks(event, oBypassEvent);
+      };
+    } else {
+      const oHandler = socket.onmessage;
+      socket.onmessage = function onMessageScript(event) {
+        const data = JSON.parse(event.data);
+        const run = !eventManager.emit('PreGameEvent', data, data.action === 'getResult').canceled;
+        if (run) oHandler(event);
+        eventManager.emit('GameEvent', data);
+      };
+    }
   })();
 });
