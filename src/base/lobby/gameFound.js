1bb7d955a6c9e89f49c8296136ce22327aa73796
feildmaster <git@feildmaster.com>
2019-01-03 10:15:34 -0600

    chore: Game found on play page only

--- src/base/lobby/gameFound.js
+++ src/base/lobby/gameFound.js
@@ -1,4 +1,4 @@
-(() => {
+onPage('Play', () => {
   const title = document.title;
   eventManager.on('getWaitingQueue', function updateTitle() {
     // Title has been modified
@@ -9,4 +9,4 @@
     document.title = title;
   });
   fn.infoToast('The page title now changes when a match is found.', 'undercards.notice.play.title', '1');
-})();
+});
