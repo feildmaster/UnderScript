fdf6929186731a00fe71104067d49cdd94a5da33
feildmaster <git@feildmaster.com>
2019-01-09 09:41:30 -0600

    fix: Hide spy glitch sooner

--- src/base/game/hideSpells.js
+++ src/base/game/hideSpells.js
@@ -1,8 +1,8 @@
-eventManager.on('getTurnEnd', function hideSpells() {
+eventManager.on('getTurnEnd getTurnStart', function hideSpells() {
   // Fixes a bug with "mines" and any other potential cards that don't get cleared correctly.
-  const spells = $('#board .spellPlayed');
+  const spells = $('.spellPlayed');
   if (spells.length) {
     spells.remove();
-    debug('Removed spell')
+    debug(`(${this.event}) Removed spell`)
   }
 });
