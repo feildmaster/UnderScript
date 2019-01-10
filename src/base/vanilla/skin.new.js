dda1e1eb2285515b8d1359b0bcb46d03732eb5f1
feildmaster <git@feildmaster.com>
2019-01-10 14:20:37 -0600

    fix: Support card skin toast when there's no message present

--- src/base/vanilla/skin.new.js
+++ src/base/vanilla/skin.new.js
@@ -8,8 +8,11 @@ settings.register({
 onPage('', function skins() {
   eventManager.on(':loaded', () => {
     if (settings.value('underscript.disable.skintoast')) return;
-    $el.text.contains(document.querySelectorAll('p'), 'New skins').forEach((el) => {
-      const title = el.textContent.trim();
+    [...document.querySelectorAll('p')].forEach((el) => {
+      if (!$el.text.contains(el, 'New skins') && !el.querySelectorAll('img[class*="cardSkin"]').length) {
+        return;
+      }
+      const title = el.textContent.trim() || 'New skins / avatars !';
       const images = [];
       const imageNames = [];
       el.querySelectorAll('img').forEach(({outerHTML, src}) => {
