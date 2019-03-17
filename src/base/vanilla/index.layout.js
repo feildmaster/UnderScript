79727a7530191de3a42d40c1de6073a4d7a71be8
feildmaster <git@feildmaster.com>
2019-03-17 13:46:26 -0500

    fix: Index not resizing

--- src/base/vanilla/index.layout.js
+++ src/base/vanilla/index.layout.js
@@ -2,13 +2,14 @@ settings.register({
   name: 'Disable Game List Resizing',
   key: 'underscript.disable.adjustSpectateView',
   refresh: () => onPage(''),
+  category: 'Home',
 });
 
 onPage('', function adjustSpectateView() {
   if (settings.value('underscript.disable.adjustSpectateView')) return;
   eventManager.on(':load', () => {
-    const spectate = $('.spectateTable');
-    const tbody = $('.spectateTable tbody');
+    const spectate = $('#liste');
+    const tbody = spectate.find('tbody');
     const footer = $('.mainContent footer');
     function doAdjustment() {
       tbody.css({height: 'auto', 'max-height': `${footer.offset().top - spectate.offset().top}px`});
