39b7aa6e9996ee989d80a538a68b7c1312e35905
feildmaster <git@feildmaster.com>
2019-03-30 23:16:48 -0500

    refactor: send chat message is now an event

--- src/base/game/@opponent.js
+++ src/base/game/@opponent.js
@@ -2,22 +2,13 @@ onPage('Game', () => {
   const regex = /(^| )@o\b/gi;
   let toast = fn.infoToast('You can mention opponents with @o', 'underscript.notice.mention', '1');
 
-  function convert() {
+  function convert({input}) {
     if (toast) {
       toast.close('processed');
       toast = null;
     }
-    $(this).val($(this).val()
+    $(input).val($(input).val()
       .replace(regex, `$1@${$('#enemyUsername').text()}`));
   }
-  function onEnter(e) {
-    if (e.which === 13) {
-      convert.call(this);
-    }
-  }
-  eventManager.on('ChatDetected', () => {
-    eventManager.on('Chat:getHistory', (data) => {
-      $(`#${data.room} input[type="text"]`).keydown(onEnter);
-    });
-  });
+  eventManager.on('Chat:send', convert);
 });
--- src/hooks/chat.js
+++ src/hooks/chat.js
@@ -6,14 +6,33 @@ eventManager.on(':loaded', () => {
     const oHandler = socketChat.onmessage;
     socketChat.onmessage = (event) => {
       const data = JSON.parse(event.data);
+      const {action} = data;
+
       // Populate chatroom names
-      if (data.action === 'getHistory') {
+      if (action === 'getHistory') {
         chatRoomNames[data.room] = data.roomName;
       }
-      if (eventManager.emit(`preChat:${data.action}`, data, true).canceled) return;
+      if (eventManager.emit(`preChat:${action}`, data, true).canceled) return;
       oHandler(event);
       eventManager.emit('ChatMessage', data);
-      eventManager.emit(`Chat:${data.action}`, data);
+      eventManager.emit(`Chat:${action}`, data);
     }
+    eventManager.on('Chat:getHistory', ({room, roomName: name}) => {
+      // Send text hook
+      const messages = $(`#${room} .chat-messages`);
+      $(`#${room} input[type="text"]`).keydown(function (e) {
+        if (e.which !== 13) return;
+
+        const data = {
+          room, name, messages,
+          input: this,
+        };
+        if (eventManager.emit('Chat:send', data, true).canceled) {
+          $(this).val('');
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      });
+    });
   }
 });
