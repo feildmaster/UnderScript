8d3de5d2162677d43a6fc6d2754cc3c22cf81777
feildmaster <git@feildmaster.com>
2019-04-07 12:53:20 -0500

    feat: Disable emotes in chat

---
+++ src/base/chat/disableEmotes.js
@@ -0,0 +1,18 @@
+wrap(function simpleText() {
+  const setting = settings.register({
+    name: 'Replace emotes with text',
+    key: 'underscript.emotes.simple',
+    page: 'Chat',
+    refresh: () => setting.value(),
+  });
+
+  function replace() {
+    if (!setting.value()) return;
+    $('.chat-messages img[title$=":"]').replaceWith(function() {
+      return this.title.substring(this.title.indexOf(':'));
+    });
+  }
+
+  eventManager.on('Chat:getHistory', replace);
+  eventManager.on('Chat:getMessage', replace);
+});
