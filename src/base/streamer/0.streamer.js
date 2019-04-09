717af1149ec692b7174f1b91051f8492b802f99d
feildmaster <git@feildmaster.com>
2019-04-08 22:19:44 -0500

    feat: Streamer mode
    Does nothing on its own. Simply manages the "state" of streamer mode

---
+++ src/base/streamer/0.streamer.js
@@ -0,0 +1,43 @@
+wrap(function streamer() {
+  const silent = 'Yes (silent)';
+  const disabled = 'No';
+  const mode = settings.register({
+    name: 'Enable?',
+    key: 'underscript.streamer',
+    options: ['Yes', silent, disabled],
+    default: disabled,
+    onChange: (val) => {
+      if (val === disabled) {
+        update(false);
+      } else {
+        menu.dirty();
+      }
+    },
+    type: 'select',
+    category: 'Streamer Mode',
+  });
+  const setting = settings.register({
+    key: 'underscript.streaming',
+    hidden: true,
+  });
+  Object.defineProperty(script, 'streaming', {
+    get: () => setting.value(),
+  });
+  menu.addButton({
+    text: () => `Streamer Mode: ${setting.value()?'On':'Off'}`,
+    hidden: () => mode.value() === disabled,
+    action: () => update(!setting.value()),
+  });
+  eventManager.on(':loaded', alert);
+
+  function alert() {
+    if (!setting.value() || mode.value() === silent) return;
+    fn.toast('Streamer Mode Active');
+  }
+
+  function update(value) {
+    setting.set(value);
+    menu.dirty();
+    alert();
+  }
+});
