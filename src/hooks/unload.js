5bd62854372b6363a50b651e1126127a0cd9b0b5
feildmaster <git@feildmaster.com>
2019-04-11 19:17:57 -0500

    chore: Don't close chat sockets

--- src/hooks/unload.js
+++ src/hooks/unload.js
@@ -1,11 +1,13 @@
 eventManager.on(':loaded', () => {
   function unload() {
     eventManager.emit(':unload');
+    /* This just causes issues, the server should be dealing with disconnects anyway
     const chat = window.socketChat;
     if (chat) {
       chat.onclose = noop;
       chat.close();
     }
+    // */
   }
   window.onbeforeunload = unload;
 });
