28275dd5275c953acbcf396594ce71c9cbc6a7c4
feildmaster <git@feildmaster.com>
2019-08-24 07:53:03 -0500

    feat: Indicate missing cards/artifacts in hub

---
+++ src/base/hub/missingImport.js
@@ -0,0 +1,40 @@
+wrap(function() {
+  style.add(
+    '.missingArt { color: orange; }',
+    '.missing { color: red; }',
+  );
+
+  function init() {
+    globalSet('showPage', function (page) {
+      this.super(page);
+      thing(page * 10);
+    });
+    fn.dismissable({
+      title: 'Did you know?',
+      text: `An <span class="missingArt">orange arrow</span> means you're missing artifact(s) and a <span class="missing">red arrow</span> means you're missing card(s)`,
+      key: 'underscript.notice.hubImport',
+    });
+  }
+
+  function thing(start) {
+    const pages = global('pages');
+    for (let i = start; i < start + 10 && i < pages.length; i++) {
+      check(pages[i]);
+    }
+  }
+
+  function check({code, id}) {
+    const checkArt = global('ownArtifactHub');
+    const deck = JSON.parse(atob(code));
+    const missingCard = getOwnedCardsArrayHub(deck).some((a) => !a);
+    const missingArt = deck.artifactIds.some(({id}) => !checkArt(id));
+
+    $(`#hub-deck-${id} .show-button`)
+      .toggleClass('missingArt', missingArt)
+      .toggleClass('missing', missingCard);
+  }
+
+  onPage('Hub', () => {
+    eventManager.on(':loaded', init);
+  });
+});
