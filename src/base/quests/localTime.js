704078caa2a7a336c3f91b6d48b183e501a02480
feildmaster <git@feildmaster.com>
2019-08-26 07:03:57 -0500

    fix: Local time now displays consistently

--- src/base/quests/localTime.js
+++ src/base/quests/localTime.js
@@ -1,6 +1,10 @@
 onPage('Quests', function localTime() {
   eventManager.on(':load', () => {
-    const time = luxon.DateTime.fromObject({ hour: 6, minute: 0, zone: 'Europe/Paris' }).toLocal();
-    $('[data-i18n="[html]quests-reset"],[data-i18n="[html]quests-day"]').append(` (${time.toLocaleString(luxon.DateTime.TIME_SIMPLE)} local)`)
+    setTimeout(updateTime);
   });
+
+  function updateTime() {
+    const time = luxon.DateTime.fromObject({ hour: 6, minute: 0, zone: 'Europe/Paris' }).toLocal();
+    $('[data-i18n="[html]quests-reset"],[data-i18n="[html]quests-day"]').append(` (${time.toLocaleString(luxon.DateTime.TIME_SIMPLE)} local)`);
+  }
 });
