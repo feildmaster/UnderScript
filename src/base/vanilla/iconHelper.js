e62638dc01ea2fa1228b0e6835dda7a43e087bdf
feildmaster <git@feildmaster.com>
2019-09-02 08:50:44 -0500

    feat: Icon helper

---
+++ src/base/vanilla/iconHelper.js
@@ -0,0 +1,34 @@
+wrap(() => {
+  const icons = {
+    'gold': 'Gold',
+    'dust': 'Dust<hr>Used to craft cards',
+    'pack': 'Undertale Pack',
+    'packPlus': 'Undertale Pack',
+    'drPack': 'Deltarune Pack',
+    'drPackPlus': 'Deltarune Pack',
+    'shinyPack.gif': 'Shiny Pack<hr>All cards are shiny',
+    'superPack.gif': 'Super Pack<hr>Contains: <ul><li>Common x1</li><li>Rare x1</li><li>Epic x1</li><li>Legendary x1</li></ul>',
+    'finalPack.gif': 'Final Pack<hr>Contains: <ul><li>Rare x1</li><li>Epic x1</li><li>Legendary x1</li><li>Determination x1</li></ul>',
+  };
+
+  eventManager.on(':loaded', () => {
+    fn.each(icons, (text, type) => {
+      makeTip(`img[src="images/icons/${type}${!~type.indexOf('.') ? '.png' : ''}"]`, text);
+    });
+  });
+
+  function makeTip(selector, content) {
+    tippy(selector, {
+      content,
+      theme: 'undercards info',
+      animateFill: false,
+      a11y: false,
+      ignoreAttributes: true,
+      //placement: 'left',
+    });
+  }
+  style.add(
+    '.info-theme hr { margin: 5px 0; }',
+    '.info-theme hr + * {text-align: left;}',
+  );
+});
