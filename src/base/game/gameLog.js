72e2b19a655dccf18648735b369a5ce4fbf456fc
feildmaster <feildmaster@users.noreply.github.com>
2019-10-15 16:48:02 -0500

    feat: setting to disable in-game log

--- src/base/game/gameLog.js
+++ src/base/game/gameLog.js
@@ -1,9 +1,40 @@
-style.add(
-  '#game-history { width: 75px; left: -66px; top: 70px; bottom: 70px; overflow-y: auto; right: initial; }',
-  '#game-history::-webkit-scrollbar { width: 8px; background-color: unset;  }',
-  '#game-history::-webkit-scrollbar-thumb { background-color: #555; }',
-);
+wrap(() => {
+  style.add(
+    '#game-history.left { width: 75px; left: -66px; top: 70px; overflow-y: auto; right: initial; height: 454px; }',
+    '#game-history.left::-webkit-scrollbar { width: 8px; background-color: unset;  }',
+    '#game-history.left::-webkit-scrollbar-thumb { background-color: #555; }',
+    '#game-history.hidden { display: none; }',
+  );
 
-eventManager.on('getBattleLog', (data) => {
-  // appendBattleLog
+  const BattleLogSetting = 'underscript.disable.logger';
+  const setting = settings.register({
+    name: 'Disable Undercards Battle Log',
+    key: 'underscript.disable.gamelog',
+    page: 'Game',
+    onChange: (to) => {
+      if (gameActive) toggle(to);
+    },
+  });
+  let gameActive = false;
+
+  eventManager.on('GameStart', () => {
+    gameActive = true;
+    eventManager.on(':load', () => {
+      if (setting.value()) toggle(true);
+      if (!settings.value(BattleLogSetting)) toggle(true, 'left');
+    });
+
+    settings.on(BattleLogSetting, (to) => {
+      toggle(!to, 'left');
+    });
+
+    eventManager.on('getBattleLog', (data) => {
+      // appendBattleLog
+    });
+  });
+
+  function toggle(to, clazz = 'hidden') {
+    console.log(`Toggling ${clazz}: ${to}`);
+    $('#game-history').toggleClass(clazz, to);
+  }
 });
