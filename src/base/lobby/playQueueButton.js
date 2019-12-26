be50b92fc1cc42f6f1ff05a16dfa77b38c5c8178
feildmaster <feildmaster@users.noreply.github.com>
2019-12-26 16:53:48 -0600

    chore: Update queue buttons for next version

--- src/base/lobby/playQueueButton.js
+++ src/base/lobby/playQueueButton.js
@@ -6,8 +6,9 @@ onPage("Play", function () {
     debug('jquery');
     restarting = $('p.infoMessage:contains("The server will restart in")').length !== 0;
     if (disable || restarting) {
-      queues = $("button.btn.btn-primary");
-      queues.prop("disabled", true);
+      queues = $("#standard-mode, #ranked-mode, button.btn.btn-primary");
+      queues.prop("disabled", true); // TODO: Cleanup
+      queues.toggleClass('closed', true);
       if (restarting) {
         queues.parent().hover(hover.show('Joining is disabled due to server restart.'));
       } else {
@@ -22,7 +23,8 @@ onPage("Play", function () {
     disable = false;
     if (queues && !restarting) {
       queues.parent().off('.script');
-      queues.prop("disabled", false);
+      queues.prop("disabled", false); // TODO: Cleanup
+      queues.toggleClass('closed', false);
       hover.hide();
     }
   });
