37fe79125aed4db44c3b7b757ebac46652b53225
feildmaster <feildmaster@users.noreply.github.com>
2019-12-30 14:33:13 -0600

    feat: friend color customization

--- src/base/chat/tag.friend.js
+++ src/base/chat/tag.friend.js
@@ -1,32 +1,48 @@
-settings.register({
-  name: 'Highlight <span style="color: #b1bfbe">friends</span> in chat',
-  key: 'underscript.tag.friend',
-  default: true,
-  page: 'Chat',
-});
+wrap(() => {
+  const tag = settings.register({
+    name: 'Highlight <span class="friend">friends</span> in chat',
+    key: 'underscript.tag.friend',
+    default: true,
+    page: 'Chat',
+  });
 
-eventManager.on('ChatDetected', function friendWrapper() {
-  style.add('.friend { color: #b1bfbe !important; }');
-  let toast;
-  function processMessage(message, room) {
-    if (!settings.value('underscript.tag.friend')) return;
-    if (isFriend(message.user.id)) {
-      if (!toast) {
-        toast = fn.infoToast('<span class="friend">Friends</span> are now highlighted in chat.', 'underscript.notice.highlighting', '1');
-      }
-      $(`#${room} #message-${message.id} .chat-user`).addClass('friend');
-      if (message.me) { // emotes
-        $(`#${room} #message-${message.id} .chat-message`).addClass('friend');
+  const color = settings.register({
+    name: 'Friend color',
+    key: 'underscript.tag.friend.color',
+    type: 'color',
+    default: '#b1bfbe',
+    page: 'Chat',
+    onChange: setColor,
+  });
+
+  setColor(color.value());
+
+  eventManager.on('ChatDetected', function friendWrapper() {
+    let toast;
+    function processMessage(message, room) {
+      if (!tag.value()) return;
+      if (isFriend(message.user.id)) {
+        if (!toast) {
+          toast = fn.infoToast('<span class="friend">Friends</span> are now highlighted in chat.', 'underscript.notice.highlighting', '1');
+        }
+        $(`#${room} #message-${message.id} .chat-user`).addClass('friend');
+        if (message.me) { // emotes
+          $(`#${room} #message-${message.id} .chat-message`).addClass('friend');
+        }
       }
     }
-  }
 
-  eventManager.on('Chat:getHistory', (data) => {
-    JSON.parse(data.history).forEach((message) => {
-      processMessage(message, data.room);
+    eventManager.on('Chat:getHistory', (data) => {
+      JSON.parse(data.history).forEach((message) => {
+        processMessage(message, data.room);
+      });
+    });
+    eventManager.on('Chat:getMessage', function tagFriends(data) {
+      processMessage(JSON.parse(data.chatMessage), data.room);
     });
   });
-  eventManager.on('Chat:getMessage', function tagFriends(data) {
-    processMessage(JSON.parse(data.chatMessage), data.room);
-  });
+
+  function setColor(color) {
+    style.add(`.friend { color: ${color} !important; }`);
+  }
 });
