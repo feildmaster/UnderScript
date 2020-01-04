075d9e1fb7a9dfd20889cbdc64d7d57280acf8d5
feildmaster <feildmaster@users.noreply.github.com>
2020-01-04 16:52:46 -0600

    feat: game found volume slider

--- src/base/lobby/lowerVolume.js
+++ src/base/lobby/lowerVolume.js
@@ -1,4 +1,16 @@
-eventManager.on("getWaitingQueue", function lowerVolume() {
-  // Lower the volume, the music changing is enough as is
-  audioQueue.volume = 0.3;
+wrap(() => {
+  const volume = settings.register({
+    name: 'Game Found Volume',
+    key: 'underscript.volume.gameFound',
+    type: 'slider',
+    default: 0.3,
+    max: 1,
+    step: 0.1,
+    page: 'Lobby',
+  });
+
+  eventManager.on("getWaitingQueue", function lowerVolume() {
+    // Lower the volume, the music changing is enough as is
+    global('audioQueue').volume = parseFloat(volume.value());
+  });
 });
