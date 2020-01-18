6ba6c888ed734011a92b74272ce93e68843a5625
feildmaster <feildmaster@users.noreply.github.com>
2020-01-17 21:30:17 -0600

    feat: new style functions

--- src/utils/style.js
+++ src/utils/style.js
@@ -2,14 +2,38 @@
 const style = wrap(() => {
   const el = document.createElement('style');
   el.type = 'text/css';
-  eventManager.on(':loaded', function append() {
+  eventManager.on(':loaded', function appendStyle() {
     document.head.append(el);
   });
 
   function add(...styles) {
+    return wrapper(append(styles));
+  }
+
+  function append(styles = [], nodes = []) {
     styles.forEach((newStyle) => {
-      el.appendChild(document.createTextNode(newStyle));
+      const node = document.createTextNode(newStyle);
+      nodes.push(node);
+      el.appendChild(node);
     });
+    return nodes;
+  }
+
+  function wrapper(nodes) {
+    return {
+      remove() {
+        nodes.forEach((node) => node.remove());
+        nodes.splice(0);
+        return this;
+      },
+      replace(...styles) {
+        return this.remove().append(styles);
+      },
+      append(...styles) {
+        append(styles, nodes);
+        return this;
+      },
+    };
   }
 
   api.register('addStyle', add);
