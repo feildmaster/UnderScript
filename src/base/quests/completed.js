568a1133e4102d6515e4bf65b5f5224642d19a0c
feildmaster <feildmaster@users.noreply.github.com>
2020-01-22 01:26:10 -0600

    feat: list completed quests

---
+++ src/base/quests/completed.js
@@ -0,0 +1,32 @@
+wrap(() => {
+  if (!onPage('Quests')) return;
+  const questSelector = 'input[type="submit"][value="Claim"]:not(:disabled)';
+
+  function collectQuests() {
+    const quests = document.querySelectorAll(questSelector);
+    if (quests.length) {
+      const block = getBlock();
+      const table = block.querySelector('.questTable tbody');
+      quests.forEach((quest) => {
+        table.append(quest.parentElement.parentElement.parentElement.cloneNode(true));
+      });
+      document.querySelector('#event-list').after(block);
+    }
+  }
+
+  eventManager.on(':loaded', collectQuests);
+  // style.add('progress::before { content: attr(value) " / " attr(max); float: right; color: white; }');
+
+  function getBlock() {
+    const block = document.createElement('div');
+    const h3 = document.createElement('h3');
+    h3.classList.add('event-title');
+    h3.textContent = 'Completed Quests';
+    const table = document.createElement('table');
+    table.classList.add('table', 'questTable');
+    const tbody = document.createElement('tbody');
+    table.append(tbody);
+    block.append(h3, table);
+    return block;
+  }
+});
