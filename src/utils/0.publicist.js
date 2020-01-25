46568ed557c6f600f9ae380293d1a29dcebd1921
feildmaster <feildmaster@users.noreply.github.com>
2020-01-24 21:18:10 -0600

    fix: run publicist on page load

--- src/utils/0.publicist.js
+++ src/utils/0.publicist.js
@@ -1,4 +1,11 @@
 if (!location.host.includes('undercards.net')) {
-  if (typeof setVersion === 'function') setVersion(GM_info.script.version, GM_info.scriptHandler);
+  function setup() { // eslint-disable-line no-inner-declarations
+    if (typeof setVersion === 'function') setVersion(GM_info.script.version, GM_info.scriptHandler);
+  }
+  if (document.readyState === 'complete') {
+    setup();
+  } else {
+    window.addEventListener('load', setup);
+  }
   return; // eslint-disable-line no-useless-return
 }
