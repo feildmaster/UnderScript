8415963cd13c8bb156763c92631e4ac343188067
feildmaster <feildmaster@users.noreply.github.com>
2020-02-16 11:52:06 -0600

    fix: prevent malicious links
    I don't think it was ever an issue, but I'll fix it anyway

--- src/base/chat/newLinkify.js
+++ src/base/chat/newLinkify.js
@@ -14,7 +14,7 @@ wrap(() => {
       }
       return text.replace(regex, (i, $1) => {
         const link = `${$1.substr(0, 4) !== 'http' ? 'http://' : ''}${$1}`;
-        return `<a href="${link}" target="_blank" onclick="return link('${link}') || false;">${$1}</a>`;
+        return `<a href="${link}" target="_blank" rel="noopener" onclick="return link('${link}') || false;">${$1}</a>`;
       });
     });
   });
