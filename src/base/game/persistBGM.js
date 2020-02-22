bc2b92ff44845d9422d2118fddacf0295f73a841
feildmaster <feildmaster@users.noreply.github.com>
2020-02-22 10:49:11 -0600

    fix: persist background music

--- src/base/game/persistBGM.js
+++ src/base/game/persistBGM.js
@@ -1,59 +1,33 @@
-settings.register({
-  name: 'Persist Arena (Background and Music)',
-  key: 'underscript.persist.bgm',
-  default: true,
-  refresh: window.gameId !== undefined,
-  page: 'Game',
-});
-
-eventManager.on('GameStart', () => {
-  if (!settings.value('underscript.persist.bgm')) return;
-  let restartMusic = false;
-  eventManager.on('getGameStarted', function rememberBGM(data) {
-    const key = `underscript.bgm.${data.gameId}`;
-    const value = localStorage.getItem('numBackground');
-    sessionStorage.setItem(key, value);
-  });
-  eventManager.on('getReconnection:before connect:before', function restoreBGM(data) {
-    const key = `underscript.bgm.${data.gameId}`;
-    if (sessionStorage.getItem(key) && global('musicEnabled')) {
-      debug('disabling music');
-      restartMusic = true;
-      globalSet('musicEnabled', false);
-    }
-  });
-  eventManager.on('getReconnection connect', function restoreBGM(data) {
-    const key = `underscript.bgm.${data.gameId}`;
-    if (!sessionStorage.getItem(key)) { // Store value for refreshes
-      const value = getBGM(data.gameType);
-      debug(`set ${key} ${value}`);
-      sessionStorage.setItem(key, value);
-      return;
-    }
-    const numBackground = sessionStorage.getItem(key);
-    debug(`set ${key} ${numBackground}`);
-    $('body').css('background', `#000 url('images/backgrounds/${numBackground}.png') no-repeat`);
-    if (restartMusic) {
-      debug('restarting music');
-      globalSet('musicEnabled', true);
-      const music = new Audio(`musics/themes/${numBackground}.ogg`);
-      globalSet('music', music);
-      music.volume = 0.1;
-      music.addEventListener('ended', function repeat() {
-        this.currentTime = 0;
-        this.play();
-      }, false);
-      music.play();
-    }
+wrap(() => {
+  const setting = settings.register({
+    name: 'Persist Arena (Background and Music)',
+    key: 'underscript.persist.bgm',
+    default: true,
+    refresh: window.gameId !== undefined,
+    page: 'Game',
   });
 
-  function getBGM(gameMode) {
-    if (gameMode === 'BOSS') {
-      return global('bossBackground');
-    }
-    if (gameMode === 'TUTORIAL') {
-      return 'tuto';
-    }
-    return $('body').css('background').match(/url\(".*\/(\d+).png"\)/)[1];
-  }
+  eventManager.on('GameStart', () => {
+    eventManager.on('connect', (data) => {
+      const val = sessionStorage.getItem(`underscript.bgm.${data.gameId}`);
+      if (setting.value() && val) {
+        $('body').css('background', `#000 url('images/backgrounds/${val}.png') no-repeat`);
+      }
+    });
+
+    eventManager.on(':loaded', () => {
+      globalSet('playBackgroundMusic', function playBackgroundMusic(sound) {
+        if (setting.value()) {
+          const key = `underscript.bgm.${global('gameId')}`;
+          const background = sessionStorage.getItem(key);
+          if (background) {
+            return this.super(background);
+          }
+
+          sessionStorage.setItem(key, sound);
+        }
+        return this.super(sound);
+      });
+    });
+  });
 });
