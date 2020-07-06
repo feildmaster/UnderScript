5d0d690f9ba712b17c59648e869b76a59624f992
feildmaster <feildmaster@users.noreply.github.com>
2020-07-06 00:38:05 -0500

    chore: end turn info toast

--- src/base/hotkeys/endTurn.js
+++ src/base/hotkeys/endTurn.js
@@ -1,34 +1,61 @@
-settings.register({
-  name: 'Disable End Turn Hotkey',
-  key: 'underscript.disable.endTurn',
-  page: 'Game',
-});
-settings.register({
-  name: 'Disable End Turn with Space',
-  key: 'underscript.disable.endTurn.space',
-  disabled: () => settings.value('underscript.disable.endTurn'),
-  refresh: () => typeof gameId !== 'undefined',
-  page: 'Game',
-});
-settings.register({
-  name: 'Disable End Turn with Middle Click',
-  key: 'underscript.disable.endTurn.middleClick',
-  disabled: () => settings.value('underscript.disable.endTurn'),
-  refresh: () => typeof gameId !== 'undefined',
-  page: 'Game',
-});
-
-eventManager.on('PlayingGame', function bindHotkeys() {
-  // Binds to Space, Middle Click
-  const hotkey = new Hotkey('End turn').run((e) => {
-    if (settings.value('underscript.disable.endTurn')) return;
-    if (!$(e.target).is('#endTurnBtn') && global('userTurn') === global('userId')) global('endTurn')();
+wrap(() => {
+  const fullDisable = settings.register({
+    name: 'Disable End Turn Hotkey',
+    key: 'underscript.disable.endTurn',
+    page: 'Game',
+    category: 'Hotkeys',
   });
-  if (!settings.value('underscript.disable.endTurn.space')) {
-    hotkey.bindKey(32);
-  }
-  if (!settings.value('underscript.disable.endTurn.middleClick')) {
-    hotkey.bindClick(2);
-  }
-  hotkeys.push(hotkey);
+  const spaceDisable = settings.register({
+    name: 'Disable End Turn with Space',
+    key: 'underscript.disable.endTurn.space',
+    disabled: () => fullDisable.value(),
+    refresh: () => typeof gameId !== 'undefined',
+    page: 'Game',
+    category: 'Hotkeys',
+  });
+  const mouseDisable = settings.register({
+    name: 'Disable End Turn with Middle Click',
+    key: 'underscript.disable.endTurn.middleClick',
+    disabled: () => fullDisable.value(),
+    refresh: () => typeof gameId !== 'undefined',
+    page: 'Game',
+    category: 'Hotkeys',
+  });
+
+  eventManager.on('PlayingGame', function bindHotkeys() {
+    // Binds to Space, Middle Click
+    const hotkey = new Hotkey('End turn').run((e) => {
+      if (fullDisable.value()) return;
+      if (!$(e.target).is('#endTurnBtn') && global('userTurn') === global('userId')) global('endTurn')();
+    });
+    if (!spaceDisable.value()) {
+      hotkey.bindKey(32);
+    }
+    if (!mouseDisable.value()) {
+      hotkey.bindClick(2);
+    }
+    hotkeys.push(hotkey);
+
+    if (!fullDisable.value() && !spaceDisable.value() && !mouseDisable.value()) {
+      fn.infoToast({
+        text: 'You can skip turns with <code>space</code> and <code>middle mouse button</code>. (These can be disabled in settings)',
+        className: 'dismissable',
+        buttons: {
+          text: 'Open Settings',
+          className: 'dismiss',
+          css: {
+            border: '',
+            height: '',
+            background: '',
+            'font-size': '',
+            margin: '',
+            'border-radius': '',
+          },
+          onclick: (e) => {
+            settings.open('Game');
+          },
+        },
+      }, 'underscript.notice.endTurn.hotkeys', '1');
+    }
+  });
 });
