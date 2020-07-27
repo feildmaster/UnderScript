3bddd56dbdb5fe6f915bd7095ec8377206ff1f79
feildmaster <feildmaster@users.noreply.github.com>
2020-07-27 05:55:55 -0500

    chore: update links

--- src/meta.js
+++ src/meta.js
@@ -15,8 +15,8 @@
 // @require      https://unpkg.com/luxon@1.8.2/build/global/luxon.min.js
 // @require      https://www.googletagmanager.com/gtag/js?id=UA-38424623-4
 // @homepage     https://feildmaster.github.io/UnderScript/
-// @source       https://github.com/feildmaster/UnderScript
-// @supportURL   https://github.com/feildmaster/UnderScript/issues
+// @source       https://github.com/UCProjects/UnderScript
+// @supportURL   https://github.com/UCProjects/UnderScript/issues
 // @updateURL    https://unpkg.com/underscript/dist/undercards.meta.js
 // @downloadURL  https://unpkg.com/underscript/dist/undercards.user.js
 // @namespace    https://feildmaster.com/
