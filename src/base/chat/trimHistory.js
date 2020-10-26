23c9e0cba5ec906ceedaf859e88d2b7fe0a7c22a
feildmaster <feildmaster@users.noreply.github.com>
2020-10-26 06:01:50 -0500

    feat: trim history from memory

---
+++ src/base/chat/trimHistory.js
@@ -0,0 +1,9 @@
+eventManager.on('Chat:getMessage', (data) => {
+  if (!data.idRoom) return;
+
+  const history = global('publicChats')[data.idRoom];
+
+  if (Array.isArray(history) && history.length > 50) {
+    history.splice(0, history.length - 50);
+  }
+});
