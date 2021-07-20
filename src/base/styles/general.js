3e14557f209a13f3acc927746d7605ccdb77273f
feildmaster <feildmaster@users.noreply.github.com>
2021-07-19 21:35:49 -0500

    fix: chat is unable to shrink to 0 anymore

--- src/base/styles/general.js
+++ src/base/styles/general.js
@@ -1,4 +1,5 @@
 style.add(
   '.clickable { cursor: pointer; }',
   '.mainContent { margin-bottom: 55px; }', // Never have the footer cover the bottom of the page
+  '.chat-box { min-height: 150px; }', // Fix chat getting too small
 );
