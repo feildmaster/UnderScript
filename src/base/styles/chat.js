18b658c46044c2d6f0a3ca02b909648a8c908a41
feildmaster <feildmaster@users.noreply.github.com>
2021-08-16 01:54:20 -0500

    fix: minimize button not working

---
+++ src/base/styles/chat.js
@@ -0,0 +1,6 @@
+style.add(
+  '.chat-message { overflow-wrap: break-word; }', // Break text
+  '.chat-messages { user-select: text; }', // Always allow chat to be selected
+  '.chat-messages { height: calc(100% - 30px); }', // Fix chat window being all funky with sizes
+  '.chat-messages { min-height: 100px; }', // Fix chat getting too small
+);
--- src/base/styles/general.js
+++ src/base/styles/general.js
@@ -1,5 +1,4 @@
 style.add(
   '.clickable { cursor: pointer; }',
   '.mainContent { margin-bottom: 55px; }', // Never have the footer cover the bottom of the page
-  '.chat-box { min-height: 150px; }', // Fix chat getting too small
 );
