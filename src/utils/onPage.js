64efecf2be825cff823b0e6b329685b92638bf94
feildmaster <feildmaster@users.noreply.github.com>
2022-01-23 21:55:18 -0600

    fix: getPageName

--- src/utils/onPage.js
+++ src/utils/onPage.js
@@ -1,19 +1,21 @@
-fn.getPageName = () => {
-  const length = location.pathname.length;
-  let temp;
+function getPageName() {
+  const { pathname } = location;
+  const length = pathname.length;
+  let temp = pathname.indexOf('.');
   // eslint-disable-next-line no-cond-assign
-  if ((temp = location.pathname.indexOf('.')) === -1 && (temp = location.pathname.lastIndexOf('/')) <= 0) {
+  if (temp === -1 && (temp = pathname.lastIndexOf('/')) <= 0) {
     temp = null;
   }
-  return location.pathname.substring(1, temp || length);
-};
+  return pathname.substring(1, temp || length);
+}
 
-function onPage(name, fn) {
-  const r = fn.getPageName() === name;
-  if (typeof fn === 'function' && r) {
-    fn();
+function onPage(name, callback) {
+  const r = getPageName() === name;
+  if (typeof callback === 'function' && r) {
+    callback();
   }
   return r;
 }
 
 api.register('onPage', onPage);
+fn.getPageName = getPageName;
