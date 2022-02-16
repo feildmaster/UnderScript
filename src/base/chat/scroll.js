165c546195fe909a4d306b6d513d009442aa40fe
feildmaster <feildmaster@users.noreply.github.com>
2022-02-16 07:53:08 -0600

    chore: update scroll to match vanilla

--- src/base/chat/scroll.js
+++ src/base/chat/scroll.js
@@ -5,23 +5,23 @@ eventManager.on('ChatDetected', function override() {
   // eslint-disable-next-line consistent-return
   globalSet('scroll', function scroll(room, force = false) {
     if (!force) return this.super(room);
-    const container = $(`#${room} .chat-messages`);
+    const container = room.find(`.chat-messages`);
     container.scrollTop(container.prop('scrollHeight'));
   });
-
-  function scrollToBottom(room) {
-    global('scroll')(room, true);
-  }
-
-  // Force scrolling to bottom after loading history -- do last
-  eventManager.on('Chat:getHistory', ({ room }) => scrollToBottom(room));
-
-  // See if we should scroll this message
-  let force = false;
-  eventManager.on('preChat:getMessage', function preProcess({ room }) {
-    if (this.canceled) return;
-    const container = $(`#${room} .chat-messages`);
-    force = container.scrollTop() + 100 > container.prop('scrollHeight') - container.height();
-  });
-  eventManager.on('Chat:getMessage', ({ room }) => force && scrollToBottom(room));
 });
+
+function scrollToBottom(room) {
+  global('scroll')($(`#${room}`), true);
+}
+
+// Force scrolling to bottom after loading history -- do last
+eventManager.on('Chat:getHistory', ({ room }) => scrollToBottom(room));
+
+// See if we should scroll this message
+let force = false;
+eventManager.on('preChat:getMessage', function preProcess({ room }) {
+  if (this.canceled) return;
+  const container = $(`#${room} .chat-messages`);
+  force = container.scrollTop() + 100 > container.prop('scrollHeight') - container.height();
+});
+eventManager.on('Chat:getMessage', ({ room }) => force && scrollToBottom(room));
