ba35afb9bd7ae7691ae43d338623d42c07d90d57
feildmaster <feildmaster@users.noreply.github.com>
2022-02-26 13:39:21 -0600

    feat: roughly calculate remaining quest days

---
+++ src/base/quests/remainingTime.js
@@ -0,0 +1,11 @@
+import luxon from 'luxon';
+import eventManager from '../../utils/eventManager';
+import style from '../../utils/style';
+
+eventManager.on(':loaded:Quests', () => {
+  style.add('.dailyMissed { background: repeating-linear-gradient(45deg, red, black 0.488em); }');
+
+  const date = luxon.DateTime.now();
+  date.setZone('Europe/Paris');
+  $('#viewDaily + table td:not(.dailyClaimed)').slice(date.daysInMonth - date.day).addClass('dailyMissed');
+});
