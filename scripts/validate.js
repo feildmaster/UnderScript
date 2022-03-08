29a5d010347b86a370f4735e0d86cc693abd3459
feildmaster <feildmaster@users.noreply.github.com>
2022-03-07 18:56:45 -0600

    chore: validate version in change log

---
+++ scripts/validate.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+const { getVersionById } = require('./changelog/entries');
+const { version: target } = require('../package.json');
+
+fs.readFile('./changelog.md', (_, data) => {
+  if (!getVersionById(data, target)) {
+    console.error('Missing version:', target);
+    exec('git reset --hard', () => {
+      process.exit(1);
+    });
+  }
+});
