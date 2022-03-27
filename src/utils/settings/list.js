116760e5a6f36f6e3574381c00e7ef9fce670f55
feildmaster <feildmaster@users.noreply.github.com>
2022-03-27 00:59:53 -0500

    feat: list setting type

---
+++ src/utils/settings/list.js
@@ -0,0 +1,101 @@
+import ArraySetting from './array';
+
+export default class List extends ArraySetting {
+  constructor(name = 'list') {
+    super(name);
+  }
+
+  default(data = []) {
+    return data.map(getValue);
+  }
+
+  element(value = [], update, {
+    data = [],
+  }) {
+    // Sortable list
+    const list = $('<ol>').addClass('sortedList');
+    let dragged;
+    function dragging(e) {
+      dragged = e.target;
+    }
+    function draggedOver(e) {
+      // Only allow same list elements
+      if (!dragged || e.target.parentElement !== dragged.parentElement) return;
+      e.preventDefault(); // Allow dropping
+    }
+    function dropped(e) {
+      e.preventDefault(); // Prevent potential bugs
+      if (!dragged || e.target.parentElement !== dragged.parentElement || e.target === dragged) return;
+      // Move to new position
+      const target = list.children();
+      const from = target.index(dragged);
+      const to = target.index(e.target);
+      value.splice(to, 0, ...value.splice(from, 1));
+      update(value);
+
+      if (from < to) {
+        $(e.target).after(dragged);
+      } else {
+        $(e.target).before(dragged);
+      }
+      dragged = null;
+    }
+    function addItem(o) {
+      // Create element
+      const node = document.createElement('li');
+      node.innerText = getLabel(o);
+      node.draggable = true;
+      if (typeof o.class === 'string') {
+        node.classList.add(...o.class.trim().split(/\s+/));
+      }
+      // Add listeners
+      node.addEventListener('dragstart', dragging);
+      node.addEventListener('dragover', draggedOver);
+      node.addEventListener('drop', dropped);
+      // Add to container
+      list.append(node);
+    }
+    let dirty = false;
+    value
+      .map((o) => data.find((i) => getValue(i) === o)) // Convert value to parent data
+      .filter((o, i) => { // Filter out invalid values
+        if (!o) {
+          value.splice(i, 1); // Remove invalid value from array
+          dirty = true; // Mark dirty
+        }
+        return o;
+      })
+      .forEach(addItem);
+    if (dirty) update(value); // If we have invalid values, clean the stored data
+    if (value.length !== data.length) {
+      // TODO: This should actually be done on "this.value()", so you don't end up with missing data when doing `setting.value()`
+      // For new additions, add to the end of value list
+      data.filter((o) => !value.includes(getValue(o))).forEach((o) => {
+        value.push(getValue(o));
+        addItem(o);
+      });
+    }
+    return list;
+  }
+
+  styles() {
+    return [
+      '.sortedList { flex-basis: 100%; padding: 0; list-style-position: inside; }',
+      '.sortedList li { cursor: pointer; }',
+    ];
+  }
+}
+
+function getValue(item) {
+  if (typeof item === 'object') {
+    return item.val || item.value || getLabel(item); // Fallback to label should technically be an invalid state... but whatever
+  }
+  return item;
+}
+
+function getLabel(item) {
+  if (typeof item === 'object') {
+    return item.label || item.text;
+  }
+  return item;
+}
---
+++ src/utils/settings/types.js
@@ -0,0 +1,8 @@
+export { default as ArrayType } from './array';
+export { default as Boolean } from './boolean';
+export { default as Color } from './color';
+export { default as List } from './list';
+export { default as Password } from './password';
+export { default as Select } from './select';
+export { default as Slider } from './slider';
+export { default as Text } from './text';
