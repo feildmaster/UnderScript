c77cda73bd949794bafde5f726745f72ce1f70f0
feildmaster <feildmaster@users.noreply.github.com>
2022-04-06 04:18:25 -0500

    fix: delay loading ignore list

--- src/base/ignorelist.js
+++ src/base/ignorelist.js
@@ -1,7 +1,10 @@
 import each from '../utils/each';
+import eventManager from '../utils/eventManager';
 import ignoreUser from '../utils/ignoreUser';
 
-each(localStorage, (name, key) => {
-  if (!key.startsWith('underscript.ignore.')) return;
-  ignoreUser(name, key);
+eventManager.on(':load', () => {
+  each(localStorage, (name, key) => {
+    if (!key.startsWith('underscript.ignore.')) return;
+    ignoreUser(name, key);
+  });
 });
