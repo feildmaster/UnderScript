2d979e85442338a388845bda53764c3f39513d50
feildmaster <feildmaster@users.noreply.github.com>
2022-07-06 15:58:24 -0500

    feat: add all types of `appendCard` events

--- src/base/vanilla/card.append.js
+++ src/base/vanilla/card.append.js
@@ -1,15 +1,49 @@
+import each from '../../utils/each';
+import eventEmitter from '../../utils/eventEmitter';
 import eventManager from '../../utils/eventManager';
 import { globalSet } from '../../utils/global';
 
+const PREFIX = 'appendCard';
+const internal = eventEmitter();
+let event = PREFIX;
+let data;
+
+internal.on('set', (e = PREFIX) => {
+  event = e;
+}).on('pre', (...args) => {
+  eventManager.emit(`pre:func:${event}`, ...args);
+}).on('post', (...args) => {
+  if (event === PREFIX || data && !args.length) {
+    eventManager.emit(`func:${event}`, ...(data || args));
+    data = null;
+    event = PREFIX; // Reset
+  } else {
+    data = args;
+  }
+});
+
 eventManager.on(':loaded', () => {
-  globalSet('appendCard', function appendCard(card, container) {
-    eventManager.emit('pre:appendCard()', card);
+  const set = globalSet(PREFIX, function appendCard(card, container) {
+    internal.emit('pre', card);
     const element = this.super(card, container);
-    eventManager.emit('appendCard()', {
-      card, element,
-    });
+    internal.emit('post', card, element);
     return element;
   }, {
     throws: false,
+  });
+  if (set === undefined) return;
+
+  function override(key) {
+    globalSet(key, function func(...args) {
+      internal.emit('set', key);
+      const ret = this.super(...args);
+      internal.emit('post');
+      return ret;
+    });
+  }
+
+  each(window, (_, key = '') => {
+    if (key !== PREFIX && key.startsWith(PREFIX) ||
+        key === 'showCardHover') override(key);
   });
 });
