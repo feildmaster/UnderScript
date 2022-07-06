b97b32a1fbca88b99d91dcf4ac6f8dd31064db4d
feildmaster <feildmaster@users.noreply.github.com>
2022-07-06 15:57:39 -0500

    chore: remove double style

--- src/base/chat/style.js
+++
@@ -1,7 +0,0 @@
-import eventManager from '../../utils/eventManager';
-import style from '../../utils/style';
-
-eventManager.on('ChatDetected', () => {
-  // Fix chat window being all funky with sizes
-  style.add('.chat-messages { height: calc(100% - 30px); }');
-});
