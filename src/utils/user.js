f0eeb44534ec06fe60788162426243d0015dd215
feildmaster <feildmaster@users.noreply.github.com>
2022-07-16 20:06:56 -0500

    feat: user deck functions

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import { global } from './global';
 import * as api from './4.api';
+import eventManager from './eventManager';
+import sleep from './sleep';
 
 let selfData;
 export function self() {
@@ -30,6 +33,43 @@ function isPriority(user, priority) {
   return user.mainGroup.priority <= priority;
 }
 
+export function getCollection() {
+  return getDeckConfig().then(({ collection: data }) => parse(data));
+}
+
+export function getDecks() {
+  return getDeckConfig().then(({ decks: data }) => parse(data));
+}
+
+export function getArtifacts() {
+  return getDeckConfig().then(({ artifacts: data }) => parse(data));
+}
+
+export function getAllArtifacts() {
+  return getDeckConfig().then(({ allArtifacts: data }) => parse(data));
+}
+
+function getDeckConfig() {
+  return new Promise((res) => {
+    eventManager.on('Deck:Loaded', (data) => res(data));
+    sleep(500).then(() => res());
+  }).then((res) => {
+    if (res) return res;
+    return axios.get('DecksConfig').then(({ data }) => {
+      eventManager.singleton.emit('Deck:Loaded', data);
+      return data;
+    });
+  });
+}
+
+function parse(data) {
+  return typeof data === 'string' ? JSON.parse(data) : data;
+}
+
 const user = api.mod.user;
 user.isMod = isMod;
 user.isStaff = isStaff;
+user.getCollection = getCollection;
+user.getDecks = getDecks;
+user.getArtifacts = getArtifacts;
+user.getAllArtifacts = getAllArtifacts;
