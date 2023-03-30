c45bd02892564e0c8499e8905897669203ba95b9
feildmaster <feildmaster@users.noreply.github.com>
2023-03-30 01:25:14 -0500

    feat: check function for plugins to utilize

---
+++ src/checker.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+function checkUnderscript(pluginName) {
+  if (window.underscript) return;
+
+  const key = 'underscript.required';
+  if (sessionStorage.getItem(key)) return;
+  sessionStorage.setItem(key, '1'); // Set instantly to prevent multiple alerts happening
+  const message = "Looks like you don't have UnderScript installed, or you deactivated it! In order for plugins to work, you need to have it up and running. Until then, the features of this userscript will simply not work. Thank you for your understanding.";
+
+  let alerted = true;
+  if (window.SimpleToast) {
+    SimpleToast({
+      title: 'Missing Requirements',
+      text: message,
+      footer: pluginName,
+    });
+  } else if (window.BootstrapDialog) {
+    BootstrapDialog.show({
+      title: 'Oh No!',
+      type: BootstrapDialog.TYPE_WARNING,
+      message,
+      buttons: [{
+        label: 'Proceed',
+        cssClass: 'btn-primary',
+        action(dialog) {
+          dialog.close();
+        },
+      }],
+    });
+  } else {
+    alerted = false;
+  }
+
+  if (!alerted) sessionStorage.removeItem(key);
+
+  throw new Error(`${pluginName}: UnderScript required`);
+}
