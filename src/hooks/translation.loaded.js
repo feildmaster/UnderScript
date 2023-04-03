d5be895274a6db173dba3c7da8dc0f0112c93ab6
feildmaster <feildmaster@users.noreply.github.com>
2023-04-03 17:57:46 -0500

    fix[vanilla]: set ready state if translations fail to process correctly

--- src/hooks/translation.loaded.js
+++ src/hooks/translation.loaded.js
@@ -1,14 +1,22 @@
 import eventManager from '../utils/eventManager.js';
-import { global } from '../utils/global.js';
+import { global, globalSet } from '../utils/global.js';
 
 eventManager.on(':loaded', () => {
-  if (global('translationReady', { throws: false })) {
+  const READY = 'translationReady';
+  if (global(READY, { throws: false })) {
     eventManager.singleton.emit('translation:loaded');
   } else {
-    document.addEventListener('translationReady', () => {
+    document.addEventListener(READY, () => {
       eventManager.singleton.emit('translation:loaded');
     }, {
       once: true,
+    });
+    // Fallback for if translation function breaks
+    eventManager.on(':load', () => {
+      const translationReady = global(READY, { throws: false });
+      if (translationReady !== false) return;
+      globalSet(READY, true);
+      document.dispatchEvent(global('translationEvent'));
     });
   }
 });
