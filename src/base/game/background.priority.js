622144865495085759cc23dd37af3ce5720e481f
feildmaster <feildmaster@users.noreply.github.com>
2023-05-20 17:52:14 -0500

    chore(bg priority): add new ranks

--- src/base/game/background.priority.js
+++ src/base/game/background.priority.js
@@ -1,42 +1,60 @@
 import { infoToast } from '../../utils/2.toasts.js';
 import eventManager from '../../utils/eventManager.js';
-import { global } from '../../utils/global.js';
 import * as settigs from '../../utils/settings/index.js';
 import style from '../../utils/style.js';
 
+const NONE = 'None';
+
 const levels = [{
   text: 'Legend',
-  class: 'PRIORITY LEGEND',
+  rank: 'LEGEND',
   val: 0,
 }, {
+  text: 'Master',
+  rank: 'MASTER',
+  val: 7,
+}, {
+  text: 'Onyx',
+  rank: 'ONYX',
+  val: 8,
+}, {
   text: 'Diamond',
-  class: 'PRIORITY DIAMOND',
+  rank: 'DIAMOND',
   val: 1,
 }, {
   text: 'Ruby',
-  class: 'PRIORITY RUBY',
+  rank: 'RUBY',
   val: 2,
 }, {
   text: 'Amethyst',
-  class: 'PRIORITY AMETHYST',
+  rank: 'AMETHYST',
   val: 3,
 }, {
   text: 'Saphire',
-  class: 'PRIORITY SAPHIRE',
+  rank: 'SAPHIRE',
   val: 4,
 }, {
   text: 'Emerald',
-  class: 'PRIORITY EMERALD',
+  rank: 'EMERALD',
   val: 5,
 }, {
   text: 'Gold',
-  class: 'PRIORITY GOLD',
+  rank: 'GOLD',
   val: 6,
 }, {
-  text: 'None',
-  class: 'PRIORITY',
+  text: NONE,
   val: 100,
 }];
+
+// Match against index rather than encoded value
+levels.forEach((level, index) => {
+  level.index = index;
+  const classes = ['PRIORITY'];
+  if (level.rank) {
+    classes.push(level.rank);
+  }
+  level.class = classes.join(' ');
+});
 
 style.add(
   '.sortedList .PRIORITY.GOLD { --color: #ffe455; }',
@@ -45,9 +63,13 @@ style.add(
   '.sortedList .PRIORITY.RUBY { --color: #ff0303; }',
   '.sortedList .PRIORITY.DIAMOND { --color: #00ced2; }',
   '.sortedList .PRIORITY.AMETHYST { --color: #ff00ff; }',
+  '.sortedList .PRIORITY.ONYX { --color: #3d1f8d; }',
+  '.sortedList .PRIORITY.MASTER { --color: #ffb100; }',
   '.sortedList .PRIORITY.LEGEND { animation-name: rainbowSetting; animation-duration: 7s; animation-timing-function: linear; animation-iteration-count: infinite; }',
   '@keyframes rainbowSetting { 0% { box-shadow: inset 0px 0px 20px 1px #f00; } 17% { box-shadow: inset 0px 0px 20px 1px #ff0; } 33% { box-shadow: inset 0px 0px 20px 1px #0f0; } 50% { box-shadow: inset 0px 0px 20px 1px #0ff; } 67% { box-shadow: inset 0px 0px 20px 1px #00f; } 84% { box-shadow: inset 0px 0px 20px 1px #f0f; } 100% { box-shadow: inset 0px 0px 20px 1px #f00; } }',
   '.sortedList .PRIORITY { padding-left: 5px; margin-bottom: 5px; box-shadow: inset 0px 0px 20px 1px var(--color); }',
+  // TODO: Remove when fixed (hopefully by next month)
+  '.OLD_ONYX { -webkit-box-shadow: inset 0 0 20px 2px #3d1f8d; box-shadow: inset 0 0 20px 1px #3d1f8d; }',
 );
 
 const notify = settigs.register({
@@ -68,21 +90,27 @@ const setting = settigs.register({
 });
 
 eventManager.on('connect', (data) => {
-  if (global('spectate')) return;
-  const { oldDivision = '' } = JSON.parse(data.you);
+  // if (global('spectate')) return;
 
-  const rank = getLevel(oldDivision);
-  if (rank === -1) return; // Invalid rank
-  // Remove old rank
+  const { oldDivision = '' } = JSON.parse(data.you);
   const oldRank = getRank(oldDivision);
+  const level = getLevel(oldRank);
+  if (!level) return;
+
+  // Remove old rank
   $('#yourSide').removeClass(`OLD_${oldRank}`);
 
   // Get preferred board value
-  const value = setting.value().find((v) => v >= rank);
-  if (value === 100) return; // top setting is "none"
+  const value = setting.value().find(({ index }) => index >= level.index);
+  if (value.text === NONE) {
+    if (notify.value()) {
+      infoToast('Your board background has been disabled');
+    }
+    return;
+  }
 
   // Set new division
-  const division = levels.find(({ val: v }) => v === value).text.toUpperCase();
+  const division = value.text.toUpperCase();
   $('#yourSide').addClass(`OLD_${division}`);
   if (notify.value() && oldRank !== division) {
     infoToast(`Your board background has been set to "${division}"`);
@@ -90,16 +118,7 @@ eventManager.on('connect', (data) => {
 });
 
 function getLevel(rank = '') {
-  switch (getRank(rank)) {
-    case 'LEGEND': return 0;
-    case 'DIAMOND': return 1;
-    case 'RUBY': return 2;
-    case 'AMETHYST': return 3;
-    case 'SAPHIRE': return 4;
-    case 'EMERALD': return 5;
-    case 'GOLD': return 6;
-    default: return -1;
-  }
+  return levels.find(({ rank: text }) => text === rank);
 }
 
 function getRank(rank = '') {
