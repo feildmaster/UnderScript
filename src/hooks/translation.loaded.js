c3938cfd011dafaf6dd7358bda7c111320382a26
feildmaster <feildmaster@users.noreply.github.com>
2023-05-20 15:50:17 -0500

    chore: provide if `translation:loaded` event was delayed

--- src/hooks/translation.loaded.js
+++ src/hooks/translation.loaded.js
@@ -1,22 +1,26 @@
 import eventManager from '../utils/eventManager.js';
 import { global, globalSet } from '../utils/global.js';
 
+const READY = 'translationReady';
+let fallback;
+
 eventManager.on(':loaded', () => {
-  const READY = 'translationReady';
   if (global(READY, { throws: false })) {
     eventManager.singleton.emit('translation:loaded');
   } else {
     document.addEventListener(READY, () => {
-      eventManager.singleton.emit('translation:loaded');
+      eventManager.singleton.emit('translation:loaded', fallback);
     }, {
       once: true,
     });
-    // Fallback for if translation function breaks
-    eventManager.on(':load', () => {
-      const translationReady = global(READY, { throws: false });
-      if (translationReady !== false || !$.i18n.messageStore.messages.en) return;
-      globalSet(READY, true);
-      document.dispatchEvent(global('translationEvent'));
-    });
   }
+});
+
+// Fallback for if translation function breaks
+eventManager.on(':load', () => {
+  const translationReady = global(READY, { throws: false });
+  if (translationReady !== false || !$?.i18n.messageStore.messages.en) return;
+  fallback = true;
+  globalSet(READY, true);
+  document.dispatchEvent(global('translationEvent'));
 });
