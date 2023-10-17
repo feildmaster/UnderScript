5461e8c5fee91fb1ebefcbe8e8c07034cad512f9
feildmaster <feildmaster@users.noreply.github.com>
2023-10-17 16:13:30 -0500

    chore: improve tippy default code

--- src/base/vanilla/tippy.js
+++ src/base/vanilla/tippy.js
@@ -1,13 +1,18 @@
+import tippy from 'tippy.js';
 import eventManager from '../../utils/eventManager.js';
 import { global } from '../../utils/global.js';
 
 // todo: Setting?
-eventManager.on(':loaded', () => {
-  const tippy = global('tippy', { throws: false });
-  if (!tippy) return;
-  const defaults = tippy.setDefaultProps || tippy.setDefaults;
-  defaults({
-    theme: 'undercards',
-    animateFill: false,
+eventManager.on(':load', () => {
+  [
+    global('tippy', { throws: false }),
+    tippy,
+  ].forEach((tip) => {
+    if (!tip) return;
+    const defaults = tip.setDefaultProps || tip.setDefaults;
+    defaults({
+      theme: 'undercards',
+      animateFill: false,
+    });
   });
 });
