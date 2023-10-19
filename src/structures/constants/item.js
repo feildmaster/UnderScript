80fb652799f963b641524b54a5c0dd3dad16be32
feildmaster <feildmaster@users.noreply.github.com>
2023-10-19 09:39:21 -0500

    chore: add more item types

--- src/structures/constants/item.js
+++ src/structures/constants/item.js
@@ -1,16 +1,20 @@
 import Constant from './constant.js';
-import * as api from '../../utils/4.api.js';
 
 export default class Item extends Constant {
   static GOLD = new Item('Gold', 'gold', 'GOLD', 'reward-gold');
-  static UCP = new Item('UCP', 'ucp');
+  static UCP = new Item('UCP', 'ucp', 'reward-ucp');
   static DUST = new Item('Dust', 'dust', 'DUST', 'reward-dust');
   static UT_PACK = new Item('Pack', 'pack', 'PACK', 'reward-pack');
   static DR_PACK = new Item('DR Pack', 'dr pack', 'DRPack', 'DR_PACK', 'reward-dr-pack');
   static SHINY_PACK = new Item('Shiny Pack', 'SinyPack', 'shiny pack', 'reward-shiny-pack');
-  static SUPER_PACK = new Item('Super Pack', 'SuperPack', 'super pack');
-  static FINAL_PACK = new Item('Final Pack', 'FinalPack', 'final pack');
+  static SUPER_PACK = new Item('Super Pack', 'SuperPack', 'super pack', 'reward-super-pack');
+  static FINAL_PACK = new Item('Final Pack', 'FinalPack', 'final pack', 'reward-final-pack');
   static DT_FRAGMENT = new Item('DT Fragment', 'ragment', 'dt fragment', 'reward-dt-fragment');
+  static EMOTE = new Item('Emote', 'emote', 'reward-emote');
+  static CARD = new Item('Card', 'card');
+  static SKIN = new Item('Card Skin', 'Skin', 'card skin', 'skin', 'reward-card-skin');
+  static AVATAR = new Item('Avatar', 'avatar', 'reward-avatar');
+  static PROFILE = new Item('Profile Skin', 'Profile', 'profile skin', 'profile', 'reward-profile-skin');
 
   static find(name) {
     if (!Item.find.values) {
@@ -19,8 +23,3 @@ export default class Item extends Constant {
     return Item.find.values.find((item) => item.equals(name));
   }
 }
-
-const constants = api.mod.constants;
-Object.keys(Item).forEach((key) => {
-  constants[key] = Item[key];
-});
