72daf7f56312865e02a5ac513944a56e5b3b2a72
feildmaster <feildmaster@users.noreply.github.com>
2023-10-22 10:10:10 -0500

    chore: reorder items

--- src/structures/constants/item.js
+++ src/structures/constants/item.js
@@ -4,16 +4,18 @@ export default class Item extends Constant {
   static GOLD = new Item('Gold', 'gold', 'GOLD', 'reward-gold');
   static UCP = new Item('UCP', 'ucp', 'reward-ucp');
   static DUST = new Item('Dust', 'dust', 'DUST', 'reward-dust');
+  static DT_FRAGMENT = new Item('DT Fragment', 'ragment', 'dt fragment', 'reward-dt-fragment');
+
   static UT_PACK = new Item('Pack', 'pack', 'PACK', 'reward-pack');
   static DR_PACK = new Item('DR Pack', 'dr pack', 'DRPack', 'DR_PACK', 'reward-dr-pack');
   static SHINY_PACK = new Item('Shiny Pack', 'SinyPack', 'shiny pack', 'reward-shiny-pack');
   static SUPER_PACK = new Item('Super Pack', 'SuperPack', 'super pack', 'reward-super-pack');
   static FINAL_PACK = new Item('Final Pack', 'FinalPack', 'final pack', 'reward-final-pack');
-  static DT_FRAGMENT = new Item('DT Fragment', 'ragment', 'dt fragment', 'reward-dt-fragment');
-  static EMOTE = new Item('Emote', 'emote', 'reward-emote');
+
   static CARD = new Item('Card', 'card');
   static SKIN = new Item('Card Skin', 'Skin', 'card skin', 'skin', 'reward-card-skin');
   static AVATAR = new Item('Avatar', 'avatar', 'reward-avatar');
+  static EMOTE = new Item('Emote', 'emote', 'reward-emote');
   static PROFILE = new Item('Profile Skin', 'Profile', 'profile skin', 'profile', 'reward-profile-skin');
 
   static find(name) {
