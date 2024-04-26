35c761464fbae52f54e2caa2210da4bf430b82e9
Alan H <879790+feildmaster@users.noreply.github.com>
2024-04-25 22:44:42 -0500

    Create checkerV2.js

---
+++ src/checkerV2.js
@@ -0,0 +1,37 @@
+(function (pluginName = 'UNKNOWN') {
+  if (window.underscript) return;
+
+  const key = 'underscript.required';
+  if (!sessionStorage.getItem(key)) {
+    sessionStorage.setItem(key, '1'); // Set instantly to prevent multiple alerts happening
+    const message = "Looks like you don't have UnderScript installed, or you deactivated it! In order for plugins to work, you need to have it up and running. Until then, the features of this userscript will simply not work. Thank you for your understanding.";
+  
+    let alerted = true;
+    if (window.SimpleToast) {
+      SimpleToast({
+        title: 'Missing Requirements',
+        text: message,
+        footer: pluginName,
+      });
+    } else if (window.BootstrapDialog) {
+      BootstrapDialog.show({
+        title: 'Oh No!',
+        type: BootstrapDialog.TYPE_WARNING,
+        message,
+        buttons: [{
+          label: 'Proceed',
+          cssClass: 'btn-primary',
+          action(dialog) {
+            dialog.close();
+          },
+        }],
+      });
+    } else {
+      alerted = false;
+    }
+  
+    if (!alerted) sessionStorage.removeItem(key);
+  }
+
+  throw new Error(`${pluginName}: UnderScript required`);
+})(this.GM_info?.script?.name);
