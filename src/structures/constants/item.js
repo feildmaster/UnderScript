3d546f0a3fef3945da04e2539b518e01ca17c636
feildmaster <feildmaster@users.noreply.github.com>
2024-09-09 19:36:53 -0500

    fix: add missing item keywords

--- src/structures/constants/item.js
+++ src/structures/constants/item.js
@@ -2,16 +2,16 @@ import Constant from './constant.js';
 
 export default class Item extends Constant {
   static GOLD = new Item('Gold', 'gold', 'GOLD', 'reward-gold');
-  static UCP = new Item('UCP', 'ucp', 'reward-ucp');
-  static DUST = new Item('Dust', 'dust', 'DUST', 'reward-dust');
+  static UCP = new Item('UCP', 'ucp', 'item-ucp', 'reward-ucp');
+  static DUST = new Item('Dust', 'dust', 'DUST', 'item-dust', 'reward-dust');
   static EXP = new Item('XP', 'xp', 'exp', 'experience', 'stat-xp', 'reward-xp');
   static ELO = new Item('elo');
-  static DT_FRAGMENT = new Item('DT Fragment', 'ragment', 'dt fragment', 'reward-dt-fragment');
+  static DT_FRAGMENT = new Item('DT Fragment', 'ragment', 'dt fragment', 'item-dt-fragment', 'reward-dt-fragment');
 
   static UT_PACK = new Item('Pack', 'pack', 'PACK', 'reward-pack');
   static DR_PACK = new Item('DR Pack', 'dr pack', 'DRPack', 'DR_PACK', 'reward-dr-pack');
   static UTY_PACK = new Item('UTY Pack', 'uty pack', 'UTYPack', 'UTY_PACK', 'reward-uty-pack');
-  static SHINY_PACK = new Item('Shiny Pack', 'SinyPack', 'shiny pack', 'SHINY_PACK', 'reward-shiny-pack');
+  static SHINY_PACK = new Item('Shiny Pack', 'ShinyPack', 'shiny pack', 'SHINY_PACK', 'reward-shiny-pack');
   static SUPER_PACK = new Item('Super Pack', 'SuperPack', 'super pack', 'reward-super-pack');
   static FINAL_PACK = new Item('Final Pack', 'FinalPack', 'final pack', 'reward-final-pack');
 
@@ -22,9 +22,9 @@ export default class Item extends Constant {
   static PROFILE = new Item('Profile Skin', 'Profile', 'profile skin', 'profile', 'reward-profile-skin');
 
   static find(name) {
-    if (!Item.find.values) {
-      Item.find.values = Object.values(Item);
-    }
-    return Item.find.values.find((item) => item.equals(name));
+    // eslint-disable-next-line no-use-before-define
+    return items.find((item) => item.equals(name));
   }
 }
+
+const items = Object.values(Item);
