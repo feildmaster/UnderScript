f96519160d7c2f6831c1846d999ceb8b32caf06d
feildmaster <feildmaster@users.noreply.github.com>
2025-03-25 23:29:44 -0500

    chore(plugin): update enable module

--- src/base/plugin/enabled.local.js
+++ src/base/plugin/enabled.local.js
@@ -1,3 +1,4 @@
+import { buttonCSS } from '../../utils/1.variables.js';
 import wrap from '../../utils/2.pokemon.js';
 import { registerModule } from '../../utils/plugin.js';
 import * as settings from '../../utils/settings/index.js';
@@ -6,7 +7,7 @@ const enable = ['Enabled with toast', 'Enabled silently'];
 
 const setting = settings.register({
   name: 'New plugin behavior',
-  key: 'enable.plugins',
+  key: 'underscript.plugins.init',
   category: 'Plugins',
   data: [...enable, 'Disabled with toast', 'Disabled silently'],
   type: 'select',
@@ -16,22 +17,51 @@ wrap(() => {
   const name = 'enabled';
 
   function mod(plugin) {
-    // if (plugin.version === undefined) return;
-    const enabled = plugin.settings().add({ key: 'Enabled', default: enable.includes(setting.value()) });
+    if (!plugin.version) return;
+    const enabled = plugin.settings().add({
+      key: 'plugin.enabled',
+      name: 'Enabled',
+      default: enable.includes(setting.value()),
+    });
 
     Object.defineProperty(plugin, name, {
       get: () => enabled.value(),
     });
 
-    const registered = plugin.settings().add({ key: 'registered', hidden: true });
-    if (!registered.value()) {
-      if (setting.value().includes('toast')) {
-        // TODO: Show toast to toggle setting from default
-      } else {
-        registered.set(true);
-      }
+    const registered = plugin.settings().add({ key: 'plugin.registered', hidden: true });
+    if (registered.value()) return;
+    if (!setting.value().includes('toast')) {
+      registered.set(true);
+      return;
     }
+
+    plugin.events.on(':load', () => {
+      const isEnabled = enabled.value();
+      plugin.toast({
+        title: 'New Plugin Detected',
+        text: `"${plugin.name}" has been <span class="${isEnabled ? 'green' : 'gray'}">${isEnabled ? 'enabled' : 'disabled'}</span> by default.`,
+        error: true, // Make it red.
+        className: {
+          toast: 'dismissable',
+          button: 'dismiss',
+        },
+        buttons: [{
+          text: isEnabled ? 'Disable' : 'Enable',
+          css: buttonCSS,
+          onclick() {
+            enabled.set(!isEnabled);
+            registered.set(true);
+          },
+        }, {
+          text: 'Dismiss',
+          css: buttonCSS,
+          onclick() {
+            registered.set(true);
+          },
+        }],
+      });
+    });
   }
 
-  registerModule(name, mod, ['settings']);
+  registerModule(name, mod, ['settings', 'events']);
 });
