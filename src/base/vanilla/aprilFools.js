3d5f7efcd8165e880770212972a0762af9f2aacc
feildmaster <feildmaster@users.noreply.github.com>
2025-04-01 11:30:26 -0500

    fix: update april fools setting

--- src/base/vanilla/aprilFools.js
+++ src/base/vanilla/aprilFools.js
@@ -1,14 +1,44 @@
 import eventManager from '../../utils/eventManager.js';
 import * as settings from '../../utils/settings/index.js';
-import { global, globalSet } from '../../utils/global.js';
+import { isApril, IMAGES } from '../../utils/isApril.js';
 
-const setting = settings.register({
+const aprilFools = settings.register({
   name: 'Disable April Fools Jokes',
   key: 'underscript.disable.fishday',
   note: 'Disables *almost* everything.',
-  hidden: () => global('fish', { throws: false }) === undefined,
-  refresh: true,
+  hidden: () => !isApril(),
+  onChange() {
+    toggleFish($('body'));
+  },
 });
-if (setting.value()) {
-  eventManager.on(':preload', () => globalSet('fish', false, { throws: false }));
+
+const basePath = 'images';
+
+function toggleFish($el) {
+  const disabled = aprilFools.value();
+  const search = disabled ? IMAGES : basePath;
+  const replace = disabled ? [IMAGES, basePath] : [basePath, IMAGES];
+
+  $el.find(`img[src*="/${search}/"],img[src^="${search}/"]`).each((index, img) => {
+    img.src = img.src.replace(...replace);
+  });
+  $el.find(`[style*="url(\\"${search}/"]`).each((i, img) => {
+    img.style.background = img.style.background.replace(...replace);
+  });
+}
+
+if (isApril()) {
+  eventManager.on(':load', () => {
+    toggleFish($('body'));
+  });
+  eventManager.on('func:appendCard', (card, container) => {
+    toggleFish(container);
+  });
+  eventManager.on('Chat:getMessage', ({ chatMessage }) => {
+    const { id } = JSON.parse(chatMessage);
+    toggleFish($(`#message-${id}`));
+  });
+  eventManager.on('Chat:getHistory', ({ room }) => {
+    toggleFish($(`#${room}`));
+  });
 }
