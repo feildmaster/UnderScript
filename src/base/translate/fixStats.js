bfcc01ed0b7e7447b1f273c2049ae3b58e403e07
feildmaster <feildmaster@users.noreply.github.com>
2025-04-03 20:12:20 -0500

    feat(translation): redo stats function

---
+++ src/base/translate/fixStats.js
@@ -0,0 +1,11 @@
+import eventManager from '../../utils/eventManager.js';
+
+eventManager.on('translation:loaded', () => {
+  const CLASSES = ['cost-color', 'atk-color', 'hp-color'];
+  $.extend($.i18n.parser.emitter, {
+    stats: (nodes) => CLASSES
+      .slice(Math.max(0, 3 - nodes.length))
+      .map((clazz, i) => nodes[i].replace(/\d+/, `<span class="${clazz}">$&</span>`))
+      .join('/'),
+  });
+});
