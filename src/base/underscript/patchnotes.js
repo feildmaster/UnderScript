c5c20c22ac6059a5cb12b6692be02904a617535e
feildmaster <feildmaster@users.noreply.github.com>
2025-06-07 11:45:24 -0500

    chore: translations

--- src/base/underscript/patchnotes.js
+++ src/base/underscript/patchnotes.js
@@ -1,19 +1,38 @@
 import * as settings from 'src/utils/settings/index.js';
 import wrap from 'src/utils/2.pokemon.js';
-import cleanData from 'src/utils/cleanData.js';
 import { noop, scriptVersion } from 'src/utils/1.variables.js';
 import style from 'src/utils/style.js';
 import { toast } from 'src/utils/2.toasts.js';
 import css from 'src/utils/css.js';
+import Translation from 'src/structures/constants/translation';
+import eventManager from 'src/utils/eventManager';
 import * as changelog from './changelog.js';
 
 wrap(function patchNotes() {
   const setting = settings.register({
-    name: 'Disable Patch Notes',
+    name: Translation.Setting('patches'),
     key: 'underscript.disable.patches',
   });
+  const installed = settings.register({
+    key: 'underscript.update.installed',
+    type: 'text',
+    hidden: true,
+    converter() {
+      const key = `underscript.update.${scriptVersion}`;
+      if (localStorage.getItem(key)) {
+        localStorage.removeItem(key);
+        return scriptVersion;
+      }
+      return undefined;
+    },
+  });
 
-  cleanData('underscript.update.', scriptVersion, 'last', 'checking', 'debug');
+  if (
+    setting.value() ||
+    !scriptVersion.includes('.') ||
+    installed.value() === scriptVersion
+  ) return;
+
   style.add(css`
     #AlertToast div.uschangelog span:nth-of-type(2) {
       max-height: 300px;
@@ -25,19 +44,18 @@ wrap(function patchNotes() {
       display: none;
     }
   `);
-  if (setting.value() || !scriptVersion.includes('.')) return;
-  const versionKey = `underscript.update.${scriptVersion}`;
-  if (localStorage.getItem(versionKey)) return;
 
   changelog.get(scriptVersion, true)
-    .then(notify)
+    .then((text) => eventManager.on('underscript:ready', () => notify(text)))
     .catch(noop);
 
+  const title = Translation.Toast('patch.notes');
+
   function notify(text) {
-    localStorage.setItem(versionKey, true);
+    installed.set(scriptVersion);
     toast({
       text,
-      title: '[UnderScript] Patch Notes',
+      title: title.translate(),
       footer: `v${scriptVersion}`,
       className: 'uschangelog',
     });
