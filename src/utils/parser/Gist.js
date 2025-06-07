f424b15e2be8b839398207928d50fd251d3729f2
feildmaster <feildmaster@users.noreply.github.com>
2025-06-07 15:24:47 -0500

    fix: gist parser

--- src/utils/parser/Gist.js
+++ src/utils/parser/Gist.js
@@ -1,13 +1,25 @@
 import Base from './FileParser.js';
 
 export default class Gist extends Base {
+  #filename;
+
   constructor(updateUrl, downloadUrl) {
     const url = new URL(updateUrl);
     if (url.hostname === 'gist.github.com') {
-      url.hostname = 'gistcdn.githack.com';
+      const parts = url.pathname.split('/');
+      super(
+        `https://api.github.com/gists/${parts[2]}`,
+        downloadUrl,
+      );
+
+      this.#filename = parts.pop();
     } else {
       throw new Error('This parser is for github gists only');
     }
-    super(`${url}`, downloadUrl);
+  }
+
+  parseData({ files = {} } = {}) {
+    const file = files[this.#filename];
+    return super.parseData(file?.content);
   }
 }
