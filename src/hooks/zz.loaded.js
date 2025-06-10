5b335ea48c662b5c97cb45b560a9b0b3bd23650c
feildmaster <feildmaster@users.noreply.github.com>
2025-06-09 21:41:49 -0500

    revert: register on all pages
    this breaks custom pages used by prettycards

--- src/hooks/zz.loaded.js
+++ src/hooks/zz.loaded.js
@@ -22,7 +22,7 @@ function done() {
 
 if (location.host.includes('undercards.net')) {
   console.log(`UnderScript(v${scriptVersion}): Loaded`); // eslint-disable-line no-console
-  if (!location.pathname.includes('/', 1)) {
+  if (document.title.includes('Undercards')) {
     register();
   }
 }
